03bcda6478c1a846328346571a4ecbb5618fadd6
Christopher Ferris <chrisfer@us.ibm.com>
2016-09-28 13:45:44 -0400

    FAB-508 add eslint and perform initial linting
    Add eslint and perform initial linting.
    I left some unused dependencies intentionally as this is WIP.
    
    Change-Id: I90af29d4c304465d9267d52feefc4fa0986c8df9
    Signed-off-by: Christopher Ferris <chrisfer@us.ibm.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,18 @@
+/*eslint-env node */
 var requireDir = require('require-dir');
+var gulp = require('gulp'),
+    eslint = require('gulp-eslint');
 
 // Require all tasks in gulp/tasks, including subfolders
 requireDir('./build/tasks', { recurse: true });
+
+gulp.task('lint', function () {
+    return gulp.src(['*.js'])
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError());
+});
+
+gulp.task('default', ['lint'], function () {
+		// This will only run if the lint task is successful...
+});
