b44d558901bab686d2ce1a185203ee76aaf8d4f9
Jim Zhang <jzhang@us.ibm.com>
2016-10-17 17:39:34 -0400

    Initial implementation for logging utility
    Change-Id: Ib5e53a2acca97e632c57ff94ce0393e194d7a051
    Signed-off-by: Jim Zhang <jzhang@us.ibm.com>

--- index.js
+++ index.js
@@ -74,3 +74,43 @@ module.exports.getChain = function(chainName, create) {
 module.exports.newKeyValueStore = function(options) {
 	return utils.newKeyValueStore(options);
 };
+
+/**
+ * Configures a logger for the entire HFC SDK to use and override the default logger. Unless this method is called,
+ * HFC uses a default logger (based on winston).
+ *
+ * @param {Object} logger a logger instance that defines the following methods: debug(), info(), warn(), error() with
+ * string interpolation methods like [util.format]{@link https://nodejs.org/api/util.html#util_util_format_format}.
+ */
+module.exports.setLogger = function(logger) {
+	var err = '';
+
+	if (typeof logger.debug !== 'function') {
+		err += 'debug() ';
+	}
+
+	if (typeof logger.info !== 'function') {
+		err += 'info() ';
+	}
+
+	if (typeof logger.warn !== 'function') {
+		err += 'warn() ';
+	}
+
+	if (typeof logger.error !== 'function' ) {
+		err += 'error()';
+	}
+
+	if (err !== '') {
+		throw new Error('The "logger" parameter must be an object that implements the following methods, which are missing: ' + err);
+	}
+
+	if (global.hfc) {
+		global.hfc.logger = logger;
+	} else {
+		global.hfc = {
+			logger: logger
+		};
+	}
+};
+
