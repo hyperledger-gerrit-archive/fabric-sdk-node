17c37805b12ff86204d60b8ee0c04491a53736a4
jjjjibm <jcwolf@us.ibm.com>
2017-03-03 21:26:00 -0500

    node-SDK FAB-2337 assign PKCS11 libpath based on search
    1.Change  PKCS11 libpath assignment algorithm to
       search among common paths for the softHSM2 installation.
    2.Change hard coded user pin to be consistent with existing
       successful CI pkcs11 tests.
    2.Add tracing to fabric-sdk-node/fabric-client/lib/impl/bccsp_pkcs11.js
    3.Change fabric-sdk-node/build/tasks/test.js to
       include pkcs11.js in test-headless task.
    
    This will cause CI to execute pkcs11.js.  The tracing will help us debug
    problems.  It can be removed later.
    
    Change-Id: I0fa094142a1aadb3315ff23f45ba70b3de2f36d5
    Signed-off-by: jjjjibm <jcwolf@us.ibm.com>

--- build/tasks/test.js
+++ build/tasks/test.js
@@ -23,7 +23,6 @@ gulp.task('test', ['lint', 'pre-test'], function() {
 	return gulp.src([
 		'test/unit/**/*.js',
 		'!test/unit/util.js',
-		'!test/unit/pkcs11.js',
 		'test/integration/fabric-ca-services-tests',
 		'test/integration/orderer-chain-tests.js',
 		'test/integration/cloudant-fabricca-tests',
@@ -52,8 +51,7 @@ gulp.task('test-headless', ['lint', 'pre-test'], function() {
 
 	return gulp.src([
 		'test/unit/**/*.js',
-		'!test/unit/util.js',
-		'!test/unit/pkcs11.js'
+		'!test/unit/util.js'
 	])
 	.pipe(tape({
 		reporter: tapColorize()
