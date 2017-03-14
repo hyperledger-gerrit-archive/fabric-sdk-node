b76576bb66397e809757f934a6f4787d55f4dd3a
Jim Zhang <jzhang@us.ibm.com>
2017-03-14 14:00:18 -0400

    Add tls support to node SDK
    FAB-2722
    The network set up is such that:
    - signing keys and certs were from the cryptogen two-org
    - tls keys and certs were taken from CR 7141 because the certs
      have special CN names to use in TLS
    
    Now the entire test suite work fine, with all 495 tests passing
    
    Note that fabric-ca-client still only supports http. It's being
    worked on with FAB-1552.
    
    Change-Id: I2ea2b35f3a5b07fb10f88a16c0d35c4d3b097c0c
    Signed-off-by: Jim Zhang <jzhang@us.ibm.com>

--- test/integration/e2e/create-channel.js
+++ test/integration/e2e/create-channel.js
@@ -43,15 +43,27 @@ test('\n\n***** End-to-end flow: create channel *****\n\n', function(t) {
 	//
 	var client = new hfc();
 	var chain = client.newChain('mychannel');
-	chain.addOrderer(new Orderer(ORGS.orderer));
+
+	var caRootsPath = ORGS.orderer.tls_cacerts;
+	let data = fs.readFileSync(path.join(__dirname, caRootsPath));
+	let caroots = Buffer.from(data).toString();
+
+	chain.addOrderer(
+		new Orderer(
+			ORGS.orderer.url,
+			{
+				'pem': caroots,
+				'ssl-target-name-override': ORGS.orderer['server-hostname']
+			}
+		)
+	);
 
 	// Acting as a client in org1 when creating the channel
 	var org = ORGS.org1.name;
 
-	hfc.newDefaultKeyValueStore({
+	return hfc.newDefaultKeyValueStore({
 		path: testUtil.storePathForOrg(org)
-	})
-	.then((store) => {
+	}).then((store) => {
 		client.setStateStore(store);
 		return testUtil.getSubmitter(client, t, 'org1');
 	})
@@ -59,24 +71,15 @@ test('\n\n***** End-to-end flow: create channel *****\n\n', function(t) {
 		t.pass('Successfully enrolled user \'admin\'');
 		the_user = admin;
 
-		//FIXME: temporary fix until mspid is configured into Chain
-		the_user.mspImpl._id = ORGS.org1.mspid;
-
 		// readin the envelope to send to the orderer
-		return readFile('./test/fixtures/channel/mychannel.tx');
-	}, (err) => {
-		t.fail('Failed to enroll user \'admin\'. ' + err);
-		t.end();
-	})
-	.then((data) => {
-		t.pass('Successfully read file');
+		data = fs.readFileSync('./test/fixtures/channel/mychannel.tx');
 		var request = {
 			envelope : data
 		};
 		// send to orderer
 		return chain.createChannel(request);
 	}, (err) => {
-		t.fail('Failed to read file for channel template: ' + err);
+		t.fail('Failed to enroll user \'admin\'. ' + err);
 		t.end();
 	})
 	.then((response) => {
@@ -101,18 +104,6 @@ test('\n\n***** End-to-end flow: create channel *****\n\n', function(t) {
 		t.end();
 	});
 });
-
-function readFile(path) {
-	return new Promise(function(resolve, reject) {
-		fs.readFile(path, function(err, data) {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(data);
-			}
-		});
-	});
-}
 
 function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
