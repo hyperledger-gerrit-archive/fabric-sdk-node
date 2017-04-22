18935c0961a1e7c69c6d79991552ee00ec34e93c
Bret Harrison <beharrison@nc.rr.com>
2017-04-22 16:29:41 -0400

    [WIP] [FAB-3343] NodeSDK decode readwrite sets
    Pull in new proto files and add in decode of
    transactions to the queryTransaction method
    
    Change-Id: I1e83f9d5bfcb8a577548d899841dcfe61f7dc1ee
    Signed-off-by: Bret Harrison <beharrison@nc.rr.com>

--- test/integration/query.js
+++ test/integration/query.js
@@ -155,70 +155,30 @@ test('  ---->>>>> Query chain working <<<<<-----', function(t) {
 			return chain.queryTransaction(tx_id); //assumes the end-to-end has run first
 		}
 	}).then((processed_transaction) => {
-		// set to be able to decode grpc objects
-		var grpc = require('grpc');
-		var commonProto = grpc.load(__dirname + '/../../fabric-client/lib/protos/common/common.proto').common;
-		var transProto = grpc.load(__dirname + '/../../fabric-client/lib/protos/peer/transaction.proto').protos;
-		var proposalProto = grpc.load(__dirname + '/../../fabric-client/lib/protos/peer/proposal.proto').protos;
-		var proposalResponseProto = grpc.load(__dirname + '/../../fabric-client/lib/protos/peer/proposal_response.proto').protos;
-		var identityProto = grpc.load(__dirname + '/../../fabric-client/lib/protos/identity.proto').msp;
-		logger.info(' Chain queryTransaction() returned processed tranaction is valid='+processed_transaction.validationCode);
-		t.equals(transProto.TxValidationCode.VALID,processed_transaction.validationCode,'got back ProcessedTransaction that is a valid transaction');
-		/*
-		 * ProcessedTransaction
-		 *    - int32 validationCode
-		 *    - Envelope
-		 *       - bytes Signature
-		 *       - bytes Payload
-		 *           - Header
-		 *              - bytes SignatureHeader
-		 *              - bytes ChannelHeader
-		 *           - bytes data Transaction
-		 *               - array TransationAction
-		 *                  - bytes header SignatureHeader
-		 *                  - bytes payload ChaincodeActionPayload
-		 *                     - bytes ChaincodeProposalPayload
-		 *                        - bytes ChaincodeProposalPayload
-		 *                            - bytes input args
-		 *                     - action ChaincodeEndorsedAction
-		 *                        - bytes proposal_response_payload
-		 *                        - array Endorsement
-		 *                            - bytes endorser - Identity
-		 *                                    - mspid
-		 *                                    - bytes IdBytes
-		 */
-
-		try {
-			var payload = commonProto.Payload.decode(processed_transaction.transactionEnvelope.getPayload());
-			var channel_header = commonProto.ChannelHeader.decode(payload.header.getChannelHeader());
-			logger.info(' Chain queryTransaction - transaction ID :: %s', channel_header.tx_id);
-
-			var transaction = transProto.Transaction.decode(payload.data);
-			var actions = transaction.getActions();
-			if(actions) for(var i in actions) {
-				let transaction_action = actions[i];
-				var transaction_action_header = commonProto.SignatureHeader.decode(transaction_action.getHeader());
-				var creator = identityProto.SerializedIdentity.decode(transaction_action_header.getCreator());
-				logger.info(' Creator mspid::%s',creator.getMspid());
-
-				var chaincode_action_payload = transProto.ChaincodeActionPayload.decode(transaction_action.getPayload());
-				var chaincode_proposal_payload =
-					proposalProto.ChaincodeProposalPayload.decode(chaincode_action_payload.getChaincodeProposalPayload());
-				logger.info(' ===>>>>> proposal arguments :: ===>>>> %s',chaincode_proposal_payload.getInput().toString('utf8'));
-				var chaincode_endorsed_action = chaincode_action_payload.getAction(); //ChaincodeEndorsedAction
-				var endorsements = chaincode_endorsed_action.getEndorsements();
-				if(endorsements) for(var j in endorsements) {
-					let endorsement = endorsements[j];
-					var endorser = identityProto.SerializedIdentity.decode(endorsement.getEndorser());
-					logger.info(' Endorser mspid::%s',endorser.getMspid());
-					logger.info(' Endorser id::%s',endorser.getIdBytes().toString('utf8'));
-				}
-			}
-		}
-		catch(err) {
-			logger.error(err);
-			throw new Error(err.stack ? err.stack : err);
-		}
+		logger.info(' processed_transaction :: %j',processed_transaction);
+		t.equals('mychannel', processed_transaction.transactionEnvelope.payload.header.channel_header.channel_id,
+			'test for header channel name');
+		t.equals('Org2MSP', processed_transaction.transactionEnvelope.payload.header.signature_header.creator.Mspid,
+			'test for header channel mspid in identity');
+		t.equals('Org1MSP', processed_transaction.transactionEnvelope.payload.data.actions['0']
+			.payload.action.endorsements['0'].endorser.Mspid,
+			'test for endorser mspid in identity');
+		t.equals('Org2MSP', processed_transaction.transactionEnvelope.payload.data.actions['0'].header.creator.Mspid,
+			'test for creator mspid in identity');
+		t.equals(200, processed_transaction.transactionEnvelope.payload.data.actions['0'].payload.action
+			.proposal_response_payload.extension.response.status,
+			'test for transation status');
+		t.equals(0, processed_transaction.transactionEnvelope.payload.data.actions['0']
+			.payload.action.proposal_response_payload.extension.results.data_model,
+			'test for data model value');
+		t.equals('a', processed_transaction.transactionEnvelope.payload.data.actions['0']
+			.payload.action.proposal_response_payload.extension.results.ns_rwset['0']
+			.rwset.writes['0'].key,
+			'test for write set key value');
+		t.equals('4', processed_transaction.transactionEnvelope.payload.data.actions['0']
+			.payload.action.proposal_response_payload.extension.results.ns_rwset['0']
+			.rwset.reads[1].version.block_num.toString(),
+			'test for read set block num');
 
 		// the "primary peer" must be a peer in the same org as the app
 		// which in this case is "peer0"
@@ -244,7 +204,7 @@ test('  ---->>>>> Query chain working <<<<<-----', function(t) {
 	});
 });
 
-test('  ---->>>>> Query chain failing: GetBlockByNumber <<<<<-----', function(t) {
+test.skip('  ---->>>>> Query chain failing: GetBlockByNumber <<<<<-----', function(t) {
 	if (!queryParameters || querys.indexOf('GetBlockByNumber') >= 0) {
 		logger.info('Executing GetBlockByNumber');
 
@@ -284,7 +244,7 @@ test('  ---->>>>> Query chain failing: GetBlockByNumber <<<<<-----', function(t)
 	} else t.end();
 });
 
-test('  ---->>>>> Query chain failing: GetTransactionByID <<<<<-----', function(t) {
+test.skip('  ---->>>>> Query chain failing: GetTransactionByID <<<<<-----', function(t) {
 	if (!queryParameters || querys.indexOf('GetTransactionByID') >= 0) {
 		return hfc.newDefaultKeyValueStore({
 			path: testUtil.storePathForOrg(orgName)
@@ -322,7 +282,7 @@ test('  ---->>>>> Query chain failing: GetTransactionByID <<<<<-----', function(
 	} else t.end();
 });
 
-test('  ---->>>>> Query chain failing: GetChainInfo <<<<<-----', function(t) {
+test.skip('  ---->>>>> Query chain failing: GetChainInfo <<<<<-----', function(t) {
 	if (!queryParameters || querys.indexOf('GetChainInfo') >= 0) {
 
 		return hfc.newDefaultKeyValueStore({
@@ -362,7 +322,7 @@ test('  ---->>>>> Query chain failing: GetChainInfo <<<<<-----', function(t) {
 	} else t.end();
 });
 
-test('  ---->>>>> Query chain failing: GetBlockByHash <<<<<-----', function(t) {
+test.skip('  ---->>>>> Query chain failing: GetBlockByHash <<<<<-----', function(t) {
 	if (!queryParameters || querys.indexOf('GetBlockByHash') >= 0) {
 		return hfc.newDefaultKeyValueStore({
 			path: testUtil.storePathForOrg(orgName)
@@ -401,7 +361,7 @@ test('  ---->>>>> Query chain failing: GetBlockByHash <<<<<-----', function(t) {
 	} else t.end();
 });
 
-test('  ---->>>>> Query Installed Chaincodes working <<<<<-----', function(t) {
+test.skip('  ---->>>>> Query Installed Chaincodes working <<<<<-----', function(t) {
 	if (!queryParameters || querys.indexOf('GetInstalledChaincodes') >= 0) {
 		return hfc.newDefaultKeyValueStore({
 			path: testUtil.storePathForOrg(orgName)
@@ -455,7 +415,7 @@ test('  ---->>>>> Query Installed Chaincodes working <<<<<-----', function(t) {
 	} else t.end();
 });
 
-test('  ---->>>>> Query Instantiated Chaincodes working <<<<<-----', function(t) {
+test.skip('  ---->>>>> Query Instantiated Chaincodes working <<<<<-----', function(t) {
 	if (!queryParameters || querys.indexOf('GetInstantiatedChaincodes') >= 0) {
 		return hfc.newDefaultKeyValueStore({
 			path: testUtil.storePathForOrg(orgName)
@@ -509,7 +469,7 @@ test('  ---->>>>> Query Instantiated Chaincodes working <<<<<-----', function(t)
 	} else t.end();
 });
 
-test('  ---->>>>> Query Channels working <<<<<-----', function(t) {
+test.skip('  ---->>>>> Query Channels working <<<<<-----', function(t) {
 	if (!queryParameters || querys.indexOf('GetChannels') >= 0) {
 		return hfc.newDefaultKeyValueStore({
 			path: testUtil.storePathForOrg(orgName)
