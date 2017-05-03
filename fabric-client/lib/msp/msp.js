cfd5918e9a58451ac8280fc965fe4437f418173b
cdaughtr <cdaughtr@us.ibm.com>
2017-05-03 13:53:53 -0400

    WIP  Remove local key store from Crypto
    CryptoSuite does not need to include a store
    when used by Chain.js, so the store is removed
    from CryptoSuite and when needed the application
    will get a Client.newCryptoKeyStore(...) and
    set it on the CryptoSuite.
    
    The functions only needed by Chain (channel) are
    moved from MSP to a new ChannelMSP class which extends
    MSP.
    
    Currently have only tested Client side with integration/
    fileKeyValueStore-fabricca.js.  ChannelMSP is
    not ready for testing.
    
    Change-Id: I41168b20fd17121432f550bf26e814199a465648
    Signed-off-by: cdaughtr <cdaughtr@us.ibm.com>

--- fabric-client/lib/msp/msp.js
+++ fabric-client/lib/msp/msp.js
@@ -1,15 +1,8 @@
 'use strict';
 
 var api = require('../api.js');
-var idModule = require('./identity.js');
-var Identity = idModule.Identity;
-var SigningIdentity = idModule.SigningIdentity;
 var utils = require('../utils.js');
 var logger = utils.getLogger('msp.js');
-
-var grpc = require('grpc');
-var identityProto = grpc.load(__dirname + '/../protos/identity.proto').msp;
-var _mspConfigProto = grpc.load(__dirname + '/../protos/msp/mspconfig.proto').msp;
 
 
 /**
@@ -45,13 +38,6 @@ var MSP = class {
 		if (!config.cryptoSuite)
 			throw new Error('Parameter "config" missing required field "cryptoSuite"');
 
-		if (typeof config.signer !== 'undefined') {
-			// when constructing local msp, a signer property is required and it must be an instance of SigningIdentity
-			if (!(config.signer instanceof SigningIdentity)) {
-				throw new Error('Parameter "signer" must be an instance of SigningIdentity');
-			}
-		}
-
 		this._rootCerts = config.rootCerts;
 		this._intermediateCerts = config.intermediateCerts;
 		this._signer = config.signer;
@@ -86,81 +72,6 @@ var MSP = class {
 	}
 
 	/**
-	 * Returns a signing identity corresponding to the provided identifier
-	 * @param {string} identifier The identifier of the requested identity object
-	 * @returns {SigningIdentity}
-	 */
-	getSigningIdentity(identifier) {
-		throw new Error('Not implemented yet');
-	}
-
-	/**
-	 * Returns the default signing identity
-	 * @returns {SigningIdentity}
-	 */
-	getDefaultSigningIdentity() {
-		return this._signer;
-	}
-
-	/**
-	 * Returns the Protobuf representation of this MSP Config
-	 */
-	toProtobuf() {
-		var proto_msp_config = new _mspConfigProto.MSPConfig();
-		proto_msp_config.setType(0); //FABRIC
-		var proto_fabric_msp_config = new _mspConfigProto.FabricMSPConfig();
-		proto_fabric_msp_config.setName(this._id);
-		proto_fabric_msp_config.setRootCerts(this._rootCerts);
-		if(this._intermediateCerts) {
-			proto_fabric_msp_config.setIntermediateCerts(this._intermediateCerts);
-		}
-		if(this._admins) {
-			proto_fabric_msp_config.setAdmins(this._admins);
-		}
-		if(this._organization_units) {
-			//organizational_unit_identifiers
-			proto_fabric_msp_config.setOrganizationalUnitIdentifiers(this._organization_units);
-		}
-		proto_msp_config.setConfig(proto_fabric_msp_config.toBuffer());
-		return proto_msp_config;
-	}
-
-	/**
-	 * DeserializeIdentity deserializes an identity
-	 * @param {byte[]} serializedIdentity - A protobuf-based serialization of an object with
-	 * 	      two fields: mspid and idBytes for certificate PEM bytes
-	 * @param {boolean} storeKey - if the user should be stored in the key store. Only when
-	 *        false will a promise not be returned
-	 * @returns {Promise} Promise for an {@link Identity} instance or
-	 *           or the Identity object itself if "storeKey" argument is false
-	 */
-	deserializeIdentity(serializedIdentity, storeKey) {
-		logger.debug('importKey - start');
-		var store_key = true; //default
-		// if storing is not required and therefore a promise will not be returned
-		// then storeKey must be set to false;
-		if(typeof storeKey === 'boolean') {
-			store_key = storeKey;
-		}
-		var sid = identityProto.SerializedIdentity.decode(serializedIdentity);
-		var cert = sid.IdBytes.toBinary();
-		logger.debug('Encoded cert from deserialized identity: %s', cert);
-		if(!store_key) {
-			var publicKey =this.cryptoSuite.importKey(cert, { algorithm: api.CryptoAlgorithms.X509Certificate }, false);
-			// TODO: the id of the new Identity instance should probably be derived from the subject info in the cert?
-			var sdk_identity = new Identity('SomeDummyValue', cert, publicKey, this);
-			return sdk_identity;
-		}
-		else {
-			return this.cryptoSuite.importKey(cert, { algorithm: api.CryptoAlgorithms.X509Certificate })
-			.then((publicKey) => {
-				// TODO: the id of the new Identity instance should probably be derived from the subject info in the cert?
-				return new Identity('SomeDummyValue', cert, publicKey, this);
-			});
-		}
-	}
-
-	/**
 	 * Checks whether the supplied identity is valid
 	 * @param {Identity} id
 	 * @returns {boolean}
@@ -168,6 +79,7 @@ var MSP = class {
 	validate(id) {
 		return true;
 	}
+
 };
 
 module.exports = MSP;
