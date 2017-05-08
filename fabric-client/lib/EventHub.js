634c127d40c4f9a66c9c589c09bbfc7f1199393c
Bret Harrison <beharrison@nc.rr.com>
2017-05-08 14:08:29 -0400

    [FAB-2843] NodeSDK - Handle network issues part1
    The first phase of handling network issues will be
    for the event hub to report to the application that
    the event stream has been shutdown. The contract with
    the applications now includes an error callback similar
    to the Promise resolve and reject.
    Block decoding was enhanced so that the decoding of the
    event block could be shared by all three event types
    we provide, block, transaction, and chaincode.
    
    Change-Id: I72082693e38bf4635b5f139f43205aabee15909d
    Signed-off-by: Bret Harrison <beharrison@nc.rr.com>

--- fabric-client/lib/EventHub.js
+++ fabric-client/lib/EventHub.js
@@ -18,6 +18,7 @@
 
 var utils = require('./utils.js');
 var Remote = require('./Remote.js');
+var Block = require('./Block');
 var grpc = require('grpc');
 var HashTable = require('hashtable');
 var logger = utils.getLogger('EventHub.js');
@@ -45,17 +46,20 @@ var ChainCodeCBE = class {
 	/*
 	 * Constructs a chaincode callback entry
 	 *
-	 * @param {string} ccid chaincode id
-	 * @param {string} eventNameFilter The regex used to filter events
-	 * @param {function} cb Callback for filter matches
+	 * @param {string} ccid - chaincode id
+	 * @param {string} eventNameFilter - The regex used to filter events
+	 * @param {function} cb - Callback for filter matches
+	 * @param {function} error - Callback for connection errors
 	 */
-	constructor(ccid, eventNameFilter, cb) {
+	constructor(ccid, eventNameFilter, cb, error) {
 		// chaincode id
 		this.ccid = ccid;
 		// event name regex filter
 		this.eventNameFilter = new RegExp(eventNameFilter);
 		// callback function to invoke on successful filter match
 		this.cb = cb;
+		// callback function to invoke on a connection failure
+		this.error = error;
 	}
 };
 
@@ -75,9 +79,12 @@ var EventHub = class {
 		// hashtable of clients registered for chaincode events
 		this.chaincodeRegistrants = new HashTable();
 		// set of clients registered for block events
-		this.blockRegistrants = new Set();
+		this.block_registrant_count = 0;
+		this.blockRegistrants = new HashTable();
+		this.blockRegistrantsError = new HashTable();
 		// hashtable of clients registered for transactional events
 		this.txRegistrants = new HashTable();
+		this.txRegistrantsError = new HashTable();
 		// peer node to connect to
 		this.ep = null;
 		// grpc event client interface
@@ -149,43 +156,16 @@ var EventHub = class {
 		var eh = this; // for callback context
 		this.call.on('data', function(event) {
 			if (event.Event == 'block') {
-				eh.blockRegistrants.forEach(function(cb) {
-					cb(event.block);
-				});
-				event.block.data.data.forEach(function(transaction) {
-					try {
-						var env = _common.Envelope.decode(transaction);
-						var payload = _common.Payload.decode(env.payload);
-						var channel_header = _common.ChannelHeader.decode(payload.header.channel_header);
-						if (channel_header.type == _common.HeaderType.ENDORSER_TRANSACTION) {
-							var tx = _transProto.Transaction.decode(payload.data);
-							var chaincodeActionPayload = _ccTransProto.ChaincodeActionPayload.decode(tx.actions[0].payload);
-							var propRespPayload = _responseProto.ProposalResponsePayload
-							.decode(chaincodeActionPayload.action.proposal_response_payload);
-							var caPayload = _ccProposalProto.ChaincodeAction.decode(propRespPayload.extension);
-							var ccEvent = _ccEventProto.ChaincodeEvent.decode(caPayload.events);
-							var cbtable = eh.chaincodeRegistrants.get(ccEvent.chaincode_id);
-							if (!cbtable) {
-								return;
-							}
-							cbtable.forEach(function(cbe) {
-								if (cbe.eventNameFilter.test(ccEvent.event_name)) {
-									cbe.cb(ccEvent);
-								}
-							});
-
-						}
-					} catch (err) {
-						logger.error('Error unmarshalling transaction=', err);
-					}
-				});
+				var block = Block.decodeBlock(event.block);
+				eh._sendBlockCallbacks(block);
+				eh._sendTxCallbacks(block);
+				eh._sendChainCodeCallbacks(block);
 			}
 			else if (event.Event == 'register'){
-				//TODO use this event to verify that eventhub is ready
 				logger.debug('connect - register event received');
 			}
 			else if (event.Event == 'unregister'){
-				// TODO use this event to mark this eventhub not ready
+				if(eh.connected) eh.disconnect();
 				logger.debug('connect - unregister event received');
 			}
 			else {
@@ -193,61 +173,154 @@ var EventHub = class {
 			}
 		});
 		this.call.on('end', function() {
-			eh.call.end();
-			// clean up Registrants - should app get notified?
-			eh.chaincodeRegistrants.clear();
-			eh.blockRegistrants.clear();
-			eh.txRegistrants.clear();
+			if(eh.connected) eh.disconnect();
+		});
+		this.call.on('error', function() {
+			if(eh.connected) eh.disconnect();
 		});
 
-		// register txCallback to process txid callbacks
-		this.registerBlockEvent(this.txCallback.bind(this));
+		this._sendSignedEvent(true);
+		logger.debug('connect - end');
 	}
 
 	/**
-	 * Disconnects peer event source<p>
-	 * Note: Only use this if creating your own EventHub. The chain
-	 * class creates a default eventHub that most Node clients can
-	 * use (see eventHubConnect, eventHubDisconnect and getEventHub).
+	 * Disconnects the connection to the peer event source.
+	 * Will close all event listeners and send an `Error` to
+	 * all listeners with an "error" callback.
 	 */
 	disconnect() {
-		if (!this.connected) return;
-		this.unregisterBlockEvent(this.txCallback);
-		this.call.end();
 		this.connected = false;
+		this._closeAllCallbacks(new Error('EventHub has been shutdown'));
+		if(this.call) {
+			this._sendSignedEvent(false);
+			this.call.end();
+		}
+	}
+
+	/*
+	 * Internal method
+	 * Builds a signed event registration
+	 * and sends it to the peer's event hub.
+	 */
+	_sendSignedEvent(register) {
+		var user = this._clientContext.getUserContext();
+		var signedEvent = new _events.SignedEvent();
+		var event = new _events.Event();
+		var reg = {events: [{event_type: 'BLOCK'}]};
+
+		if(register) {
+			event.setRegister(reg);
+		}
+		else {
+			event.setUnregister(reg);
+		}
+
+		event.setCreator(user.getIdentity().serialize());
+		signedEvent.setEventBytes(event.toBuffer());
+		var sig = user.getSigningIdentity().sign(event.toBuffer());
+		signedEvent.setSignature(Buffer.from(sig));
+		this.call.write(signedEvent);
+	}
+
+	/*
+	 * Internal method to close out all callbacks
+	 * Sends an error to all registered event error callbacks
+	 */
+	_closeAllCallbacks(err) {
+		logger.debug('_closeAllCallbacks - start');
+
+		var closer = function(key, cb) {
+			logger.debug('_closeAllCallbacks - closing this callback %s',key);
+			cb(err);
+		};
+
+		logger.debug('_closeAllCallbacks - blockRegistrantsError %s',this.blockRegistrantsError.size());
+		this.blockRegistrantsError.forEach(closer);
+		this.blockRegistrants.clear();
+		this.blockRegistrantsError.clear();
+
+		logger.debug('_closeAllCallbacks - txRegistrantsError %s',this.txRegistrantsError.size());
+		this.txRegistrantsError.forEach(closer);
+		this.txRegistrants.clear();
+		this.txRegistrantsError.clear();
+
+		var cc_closer = function(key, cbtable) {
+			cbtable.forEach(function(cbe) {
+				logger.debug('_closeAllCallbacks - closing this chaincode event %s %s',cbe.ccid, cbe.eventNameFilter);
+				if(cbe.error) {
+					cbe.error(err);
+				}
+			});
+		};
+
+		logger.debug('_closeAllCallbacks - chaincodeRegistrants %s',this.chaincodeRegistrants.size());
+		this.chaincodeRegistrants.forEach(cc_closer);
+		this.chaincodeRegistrants.clear();
+	}
+
+	/*
+	 * Internal method
+	 * checks for a connection and if not connected
+	 * throws an error
+	 */
+	_checkConnection(throw_error) {
+		if(this.connected) {
+			logger.debug(' this hub %s is connected', this.ep.getUrl());
+		}
+		else {
+			logger.debug('this hub %s is not connected', this.ep.getUrl());
+			if(throw_error) {
+				throw new Error('The event hub has not been connected to the event source');
+			}
+		}
 	}
 
 	/**
 	 * Register a callback function to receive chaincode events.
-	 * @param {string} ccid string chaincode id
-	 * @param {string} eventname string The regex string used to filter events
-	 * @param {function} callback Function Callback function for filter matches
+	 * @param {string} ccid - string chaincode id
+	 * @param {string} eventname - string The regex string used to filter events
+	 * @param {function} callback - callback function for filter matches
 	 * that takes a single parameter which is a json object representation
 	 * of type "message ChaincodeEvent" from lib/proto/chaincodeevent.proto
+	 * @param {function} error - callback function to be notified when this
+	 * event hub is shutdown.
 	 * @returns {object} ChainCodeCBE object that should be treated as an opaque
 	 * handle used to unregister (see unregisterChaincodeEvent)
 	 */
-	registerChaincodeEvent(ccid, eventname, callback) {
-		if (!this.connected) return;
-		var cb = new ChainCodeCBE(ccid, eventname, callback);
+	registerChaincodeEvent(ccid, eventname, callback, error) {
+		logger.debug('registerChaincodeEvent - start');
+		if(!ccid) {
+			throw new Error('Missing "ccid" parameter');
+		}
+		if(!eventname) {
+			throw new Error('Missing "eventname" parameter');
+		}
+		if(!callback) {
+			throw new Error('Missing "callback" parameter');
+		}
+		this._checkConnection(true);
+		var cbe = new ChainCodeCBE(ccid, eventname, callback, error);
 		var cbtable = this.chaincodeRegistrants.get(ccid);
 		if (!cbtable) {
 			cbtable = new Set();
 			this.chaincodeRegistrants.put(ccid, cbtable);
-			cbtable.add(cb);
-		} else {
-			cbtable.add(cb);
 		}
-		return cb;
+		cbtable.add(cbe);
+
+		return cbe;
 	}
 
 	/**
 	 * Unregister chaincode event registration
-	 * @param {object} ChainCodeCBE handle returned from call to
+	 * @param {object} cbe - ChainCodeCBE handle return from call to
 	 * registerChaincodeEvent.
 	 */
 	unregisterChaincodeEvent(cbe) {
-		if (!this.connected) return;
+		logger.debug('unregisterChaincodeEvent - start');
+		this._checkConnection();
+		if(!cbe) {
+			throw new Error('Missing "cbe" parameter');
+		}
 		var cbtable = this.chaincodeRegistrants.get(cbe.ccid);
 		if (!cbtable) {
 			logger.debug('No event registration for ccid %s ', cbe.ccid);
@@ -264,31 +337,19 @@ var EventHub = class {
 	 * @param {function} callback Function that takes a single parameter
 	 * which is a json object representation of type "message Block"
 	 * from lib/proto/fabric.proto
+	 * @param {function} error - callback function to be notified when this
+	 * event hub is shutdown.
 	 * @returns {Promise} Promise for a successful registration, no returned values
 	 */
-	registerBlockEvent(callback) {
-		var user = this._clientContext.getUserContext();
-		if (!this.connected) throw new Error('The event hub has not been connected to the event source');
-
-		this.blockRegistrants.add(callback);
-		if (this.blockRegistrants.size == 1) {
-
-			var signedEvent = new _events.SignedEvent();
-
-			var event = new _events.Event();
-			event.setRegister({
-				events: [{
-					event_type: 'BLOCK'
-				}]
-			});
-			event.setCreator(user.getIdentity().serialize());
-
-			signedEvent.setEventBytes(event.toBuffer());
-
-			var sig = user.getSigningIdentity().sign(event.toBuffer());
-			signedEvent.setSignature(Buffer.from(sig));
-			this.call.write(signedEvent);
+	registerBlockEvent(callback, error) {
+		logger.debug('registerBlockEvent - start');
+		if(!callback) {
+			throw new Error('Missing "callback" parameter');
 		}
+		this._checkConnection(true);
+		this.block_registrant_count++;
+		this.blockRegistrants.put(this.block_registrant_count, callback);
+		if(error) this.blockRegistrantsError.put(this.block_registrant_count, error);
 	}
 
 	/**
@@ -296,50 +357,45 @@ var EventHub = class {
 	 * @param {function} callback Function to unregister
 	 */
 	unregisterBlockEvent(callback) {
+		logger.debug('unregisterBlockEvent - start');
 		var user = this._clientContext.getUserContext();
-		if (!this.connected) return;
-
-		if (this.blockRegistrants.size <= 1) {
-			var signedEvent = new _events.SignedEvent();
-
-			var event = new _events.Event();
-			event.setUnregister({
-				events: [{
-					event_type: 'BLOCK'
-				}]
-			});
-			event.setCreator(user.getIdentity().serialize());
-
-			signedEvent.setEventBytes(event.toBuffer());
-
-			var sig = user.getSigningIdentity().sign(event.toBuffer());
-			signedEvent.setSignature(Buffer.from(sig));
-
-			this.call.write(signedEvent);
+		this._checkConnection();
+		if(!callback) {
+			throw new Error('Missing "callback" parameter');
 		}
-		this.blockRegistrants.delete(callback);
+		var keys = this.blockRegistrants.keys();
+		logger.debug('unregisterBlockEvent - keys %s',keys);
+		for(var i in keys) {
+			var key = keys[i];
+			logger.debug('unregisterBlockEvent - removing key %s',key);
+			if(this.blockRegistrants.get(key) === callback ) {
+				this.blockRegistrants.remove(key);
+				this.blockRegistrantsError.remove(key);
+			}
+		}
 	}
 
 	/**
-	 * Register a callback function to receive transactional events.<p>
-	 * Note: transactional event registration is primarily used by
-	 * the sdk to track instantiate and invoke completion events. Nodejs
-	 * clients generally should not need to call directly.
+	 * Register a callback function to receive transactional events.
 	 * @param {string} txid string transaction id
 	 * @param {function} callback Function that takes a parameter which
 	 * is a json object representation of type "message Transaction"
 	 * from lib/proto/fabric.proto and a parameter which is a boolean
 	 * that indicates if the transaction is invalid (true=invalid)
+	 * @param {function} error - callback function to be notified when this
+	 * event hub is shutdown.
 	 */
-	registerTxEvent(txid, callback) {
-		logger.debug('reg txid ' + txid);
-		if(this.connected) {
-			logger.debug(' this hub %s is connected', this.ep.getUrl());
+	registerTxEvent(txid, callback, error) {
+		logger.debug('registerTxEvent txid ' + txid);
+		if(!txid) {
+			throw new Error('Missing "txid" parameter');
 		}
-		else {
-			logger.debug('this hub %s is not connected', this.ep.getUrl());
+		if(!callback) {
+			throw new Error('Missing "callback" parameter');
 		}
+		this._checkConnection(true);
 		this.txRegistrants.put(txid, callback);
+		if(error) this.txRegistrantsError.put(txid, error);
 	}
 
 	/**
@@ -347,38 +403,95 @@ var EventHub = class {
 	 * @param txid string transaction id
 	 */
 	unregisterTxEvent(txid) {
+		logger.debug('unregisterTxEvent txid ' + txid);
+		this._checkConnection();
+		if(!txid) {
+			throw new Error('Missing "txid" parameter');
+		}
 		this.txRegistrants.remove(txid);
+		this.txRegistrantsError.remove(txid);
 	}
 
-	/**
-	 * private internal callback for processing tx events
-	 * @param {object} block json object representing block of tx
-	 * from the fabric
+	/*
+	 * private internal method for processing block events
+	 * @param {object} block protobuf object
 	 */
-	txCallback(block) {
-		logger.debug('txCallback block=%s', block.header.number);
-		var eh = this;
+	_sendBlockCallbacks(block) {
+		logger.debug('_sendBlockCallbacks block=%s', block.header.number);
+		if(this.blockRegistrants.size == 0) {
+			logger.debug('_sendBlockCallbacks - no registered block events');
+			return;
+		}
+
+		// send to all registered block listeners
+		this.blockRegistrants.forEach(function(key, cb) {
+			cb(block);
+		});
+	}
+
+	/*
+	 * private internal method for processing tx events
+	 * @param {object} block protobuf object which might contain the tx from the fabric
+	 */
+	_sendTxCallbacks(block) {
+		logger.debug('_sendTxCallbacks block=%s', block.header.number);
+		if(this.txRegistrants.size() == 0) {
+			logger.debug('_sendTxCallbacks - no registered transaction events');
+			return;
+		}
+
 		var txStatusCodes = block.metadata.metadata[_common.BlockMetadataIndex.TRANSACTIONS_FILTER];
 
 		for (var index=0; index < block.data.data.length; index++) {
-			logger.debug('txCallback - trans index=%s',index);
-			try {
-				var env = _common.Envelope.decode(block.data.data[index]);
-				var payload = _common.Payload.decode(env.payload);
-				var channel_header = _common.ChannelHeader.decode(payload.header.channel_header);
-			} catch (err) {
-				logger.error('Error unmarshalling transaction from block=', err);
-				break;
-			}
-
+			logger.debug('_sendTxCallbacks - trans index=%s',index);
+			var channel_header = block.data.data[index].payload.header.channel_header;
 			var val_code = convertValidationCode(txStatusCodes[index]);
-			logger.debug('txCallback - txid=%s  val_code=%s',val_code,  channel_header.tx_id);
-			var cb = eh.txRegistrants.get(channel_header.tx_id);
+			logger.debug('_sendTxCallbacks - txid=%s  val_code=%s',val_code, channel_header.tx_id);
+			var cb = this.txRegistrants.get(channel_header.tx_id);
 			if (cb){
-				logger.debug('txCallback - about to call the transaction call back for code=%s tx=%s', val_code, channel_header.tx_id);
+				logger.debug('_sendTxCallbacks - about to call the transaction call back for code=%s tx=%s', val_code, channel_header.tx_id);
 				cb(channel_header.tx_id, val_code);
 			}
+		}
+	};
 
+	/*
+	 * private internal method for processing chaincode events
+	 * @param {object} block protobuf object which might contain the chaincode event from the fabric
+	 */
+	_sendChainCodeCallbacks(block) {
+		logger.debug('_sendChainCodeCallbacks block=%s', block.header.number);
+		if(this.chaincodeRegistrants.size() == 0) {
+			logger.debug('_sendChainCodeCallbacks - no registered chaincode events');
+			return;
+		}
+
+		for (var index=0; index < block.data.data.length; index++) {
+			logger.debug('_sendChainCodeCallbacks - trans index=%s',index);
+			try {
+				var env = block.data.data[index];
+				var payload = env.payload;
+				var channel_header = payload.header.channel_header;
+				if (channel_header.type == _common.HeaderType.ENDORSER_TRANSACTION) {
+					var tx = payload.data;
+					var chaincodeActionPayload = tx.actions[0].payload;
+					var propRespPayload = chaincodeActionPayload.action.proposal_response_payload;
+					var caPayload = propRespPayload.extension;
+					var ccEvent = caPayload.events;
+					logger.debug('_sendChainCodeCallbacks - ccEvent %s',ccEvent);
+					var cbtable = this.chaincodeRegistrants.get(ccEvent.chaincode_id);
+					if (!cbtable) {
+						return;
+					}
+					cbtable.forEach(function(cbe) {
+						if (cbe.eventNameFilter.test(ccEvent.event_name)) {
+							cbe.cb(ccEvent);
+						}
+					});
+				}
+			} catch (err) {
+				logger.error('on.data - Error unmarshalling transaction=', err);
+			}
 		}
 	};
 };
