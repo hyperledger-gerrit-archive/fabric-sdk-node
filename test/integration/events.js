c3f738f20c1dbff759fa9840769868b726af7059
Bret Harrison <beharrison@nc.rr.com>
2017-07-13 17:39:11 -0400

    [FAB-2538] NodeSDK - endorsement error msg
    Have the NodeSDK return an error message when
    there are no valid endorsements in the
    sendTransaction() api call
    
    Change-Id: I8c70b750e8e888eb8ca7090b4fd6a2c8ee5041a6
    Signed-off-by: Bret Harrison <beharrison@nc.rr.com>

--- test/integration/events.js
+++ test/integration/events.js
@@ -216,7 +216,7 @@ test('Test chaincode instantiate with event, transaction invocation with chainco
 	}).then((results) => {
 		if ( eputil.checkProposal(results)) {
 			t.pass('Successfully endorsed the instantiate chaincode proposal');
-			var tmo = 50000;
+			var tmo = 60000;
 			return Promise.all([eputil.registerTxEvent(eh, request.txId.getTransactionID().toString(), tmo),
 				eputil.sendTransaction(channel, results)]);
 		} else {
