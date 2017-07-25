a9d4cebbf893417bde2ac424b7337b31c80d4593
Bret Harrison <beharrison@nc.rr.com>
2017-07-25 11:25:18 -0400

    [FAB-5457] NodeSDK - block decode fails
    Blocks may not have the range queries info
    values and the decode code  should be able
    to handle.
    
    Change-Id: I205931a1cae495776866d2e5135147421565b1b8
    Signed-off-by: Bret Harrison <beharrison@nc.rr.com>

--- fabric-client/lib/BlockDecoder.js
+++ fabric-client/lib/BlockDecoder.js
@@ -1247,7 +1247,7 @@ function decodeRangeQueryInfo(proto_range_query_info) {
 	range_query_info.reads_info = {};
 	// reads_info is one of QueryReads
 	let proto_raw_reads = proto_range_query_info.getRawReads();
-	if (proto_raw_reads.kv_reads) {
+	if (proto_raw_reads) {
 		range_query_info.reads_info.kv_reads = [];
 		for (let i in proto_raw_reads.kv_reads) {
 			range_query_info.reads_info.kv_reads.push(proto_raw_reads.kv_reads[i]);
@@ -1255,7 +1255,7 @@ function decodeRangeQueryInfo(proto_range_query_info) {
 	}
 	// or QueryReadsMerkleSummary
 	let proto_reads_merkle_hashes = proto_range_query_info.getReadsMerkleHashes();
-	if (proto_reads_merkle_hashes.max_degree) {
+	if (proto_reads_merkle_hashes) {
 		range_query_info.reads_merkle_hashes = {};
 		range_query_info.reads_merkle_hashes.max_degree = proto_reads_merkle_hashes.getMaxDegree();
 		range_query_info.reads_merkle_hashes.max_level = proto_reads_merkle_hashes.getMaxLevel();
