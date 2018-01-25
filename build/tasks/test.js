1e8c6e15b81ba5487af7af12929060dc1b3dec41
zhaochy <zhaochy_2015@hotmail.com>
2018-01-25 15:34:13 +0800

    [FAB-6628] 2. Support affiliation REST API
    This change implements the different REST API call to the affiliation
    endpoint on the fabric-ca server.
    
    Change-Id: I9d14c21c0c1218b3e7c595c882396aafcb32c9c6
    Signed-off-by: zhaochy <zhaochy_2015@hotmail.com>

--- build/tasks/test.js
+++ build/tasks/test.js
@@ -55,7 +55,11 @@ process.env.DOCKER_IMG_TAG = dockerImageTag;
 gulp.task('pre-test', function() {
 	return gulp.src([
 		'fabric-client/lib/**/*.js',
-		'fabric-ca-client/lib/FabricCAClientImpl.js'])
+		'fabric-ca-client/lib/FabricCAClientImpl.js',
+		'fabric-ca-client/lib/helper.js',
+		'fabric-ca-client/lib/IdentityService.js',
+		'fabric-ca-client/lib/AffiliationService.js',
+	])
 	.pipe(istanbul())
 	.pipe(istanbul.hookRequire());
 });
@@ -103,6 +107,7 @@ gulp.task('test', ['clean-up', 'lint', 'pre-test', 'docker-ready', 'ca'], functi
 		// channel: mychannel, chaincode: end2endnodesdk:v0/v1
 		'test/integration/e2e.js',
 		'test/integration/query.js',
+		'test/integration/fabric-ca-affiliation-service-tests.js',
 		'test/integration/fabric-ca-identity-service-tests.js',
 		'test/integration/fabric-ca-services-tests.js',
 		'test/integration/client.js',
