44f20f86ec04c61741fae7be16eef31c8b7f398c
davidliu <david-khala@hotmail.com>
2018-03-13 00:31:42 +0800

    [FAB-8791] refactor:using ES6 class in hash.js
    This fixed #FAB-8791
    
    Besides, also quick fix eslint error in utils.js and hash.js
    
    Change-Id: Ib090a37abd765b67ad9b6333b31d76fb7839766d
    Signed-off-by: davidliu <david-khala@hotmail.com>

--- fabric-client/lib/hash.js
+++ fabric-client/lib/hash.js
@@ -17,156 +17,115 @@
 /**
  * Implement hash primitives.
  */
-var sjcl = require('sjcl');
-var sjcl_codec = require('sjcl-codec');
-var jssha = require('jssha');
+const sjcl = require('sjcl');
 
-var jsSHA3 = require('js-sha3');
-var sha3_256 = jsSHA3.sha3_256;
-var sha3_384 = jsSHA3.sha3_384;
-var shake_256 = jsSHA3.shake_256;
-var crypto = require('crypto');
+const jsSHA3 = require('js-sha3');
+const sha3_256 = jsSHA3.sha3_256;
+const sha3_384 = jsSHA3.sha3_384;
+const shake_256 = jsSHA3.shake_256;
+const crypto = require('crypto');
+const utils = require('./utils');
 
-hash_sha2_256 = function (hash) {
-
-	if (hash) {
-		this._hash = hash._hash;
+const { bitsToBytes } = utils;
+class hash_sha2_256 {
+	constructor(hash) {
+		this.blockSize = 512;
+		if (hash) {
+			this._hash = hash._hash;
+		}
+		else {
+			this.reset();
+		}
 	}
-	else {
-		this.reset();
+	hash(data) {
+		return (new sjcl.hash.sha256()).update(data).finalize();
 	}
-};
-
-hash_sha2_256.hash = function (data) {
-	return (new sjcl.hash.sha256()).update(data).finalize();
-};
-
-hash_sha2_256.prototype = {
-
-	blockSize: 512,
-
-	reset: function () {
+	reset() {
 		this._hash = new sjcl.hash.sha256();
 		this._hash.reset();
-	},
-
-	update: function (data) {
+	}
+	update(data) {
 		this._hash.update(data);
 		return this;
-	},
-
-	finalize: function () {
-		var hash = this._hash.finalize();
+	}
+	finalize() {
+		const hash = this._hash.finalize();
 		this.reset();
 		return hash;
-
 	}
-};
+}
 
 
-var hash_sha3_256 = function (hash) {
 
-	if (hash) {
-		this._hash = hash._hash;
+class hash_sha3_256 {
+	constructor(hash) {
+		this.blockSize = 1088;
+		if (hash) {
+			this._hash = hash._hash;
+		}
+		else {
+			this.reset();
+		}
 	}
-	else {
-		this.reset();
+	hash(data) {
+		const hashBits = sjcl.codec.hex.toBits(sha3_256(bitsToBytes(data)));
+		return hashBits;
 	}
-};
-
-hash_sha3_256.hash = function (data) {
-	var hashBits = sjcl.codec.hex.toBits(sha3_256(bitsToBytes(data)));
-	return hashBits;
-};
-
-hash_sha3_256.prototype = {
-
-	blockSize: 1088,
-
-	reset: function () {
+	reset() {
 		this._hash = sha3_256.create();
-	},
-
-	update: function (data) {
+	}
+	update(data) {
 		this._hash.update(bitsToBytes(data));
 		return this;
-	},
-
-	finalize: function () {
-		var hash = this._hash.hex();
-		var hashBits = sjcl.codec.hex.toBits(hash);
+	}
+	finalize() {
+		const hash = this._hash.hex();
+		const hashBits = sjcl.codec.hex.toBits(hash);
 		this.reset();
 		return hashBits;
-
 	}
-};
 
-var hash_sha3_384 = function (hash) {
+}
 
-	if (hash) {
-		this._hash = hash._hash;
+
+class hash_sha3_384 {
+	constructor(hash) {
+		this.blockSize = 832;
+		if (hash) {
+			this._hash = hash._hash;
+		}
+		else {
+			this.reset();
+		}
 	}
-	else {
-		this.reset();
+	hash(data) {
+		return sjcl.codec.hex.toBits(sha3_384(bitsToBytes(data)));
 	}
-};
-
-hash_sha3_384.hash = function (data) {
-	var hashBits = sjcl.codec.hex.toBits(sha3_384(bitsToBytes(data)));
-	return hashBits;
-};
-
-hash_sha3_384.prototype = {
-
-	blockSize: 832,
-
-	reset: function () {
+	reset() {
 		this._hash = sha3_384.create();
-	},
-
-	update: function (data) {
+	}
+	update(data) {
 		this._hash.update(bitsToBytes(data));
 		return this;
-	},
-
-	finalize: function () {
-		var hash = this._hash.hex();
-		var hashBits = sjcl.codec.hex.toBits(hash);
-		//debug('finalize hashBits:\n',hashBits)
+	}
+	finalize() {
+		const hash = this._hash.hex();
+		const hashBits = sjcl.codec.hex.toBits(hash);
 		this.reset();
 		return hashBits;
-
 	}
-};
-
-/*
- * Convert from a bitArray to bytes (using SJCL's codec)
- * @param {bits} a bitArray to convert from
- * @return {bytes} the bytes converted from the bitArray
- */
-bitsToBytes = function (bits) {
-	return sjcl_codec.bytes.fromBits(bits);
-};
-
-/*
- * Convert from bytes to a bitArray (using SJCL's codec)
- * @param {bytes} a bytes to convert from
- * @return {bitArray} the bitArray converted from bytes
- */
-bytesToBits = function (bytes) {
-	return sjcl_codec.bytes.toBits(bytes);
-};
+}
 
 exports.hash_sha3_256 = hash_sha3_256;
 exports.hash_sha3_384 = hash_sha3_384;
 exports.hash_sha2_256 = hash_sha2_256;
 exports.sha2_256 = function (data) {
-	var sha256 = crypto.createHash('sha256');
+	const sha256 = crypto.createHash('sha256');
 	return sha256.update(data).digest('hex');
 };
 exports.sha3_256 = sha3_256;
-exports.sha2_384 = function (data){
-	var sha384 = crypto.createHash('sha384');
+exports.sha2_384 = function (data) {
+	const sha384 = crypto.createHash('sha384');
 	return sha384.update(data).digest('hex');
 };
 exports.sha3_384 = sha3_384;
