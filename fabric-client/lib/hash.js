22e052fc88e06d91ca7d518f98014aff7ad2fa23
davidliu <david-khala@hotmail.com>
2018-03-13 00:31:42 +0800

    [FAB-8791] refactor:using ES6 class in hash.js
    This fixed #FAB-8791
    
    Besides, also quick fix eslint error in utils.js and hash.js
    
    remove unused _hashFunctionKeyDerivation
    
    created sha class:
     - hash_sha2_384
     - hash_sha3_384
     - hash_sha2_256
     - hash_sha3_256
    in these classes, get rid of bitsArray and bitArray as input/output
     which are hard for test/logging
    
    created test/unit/hash.js for unit test
    
    Change-Id: Ib090a37abd765b67ad9b6333b31d76fb7839766d
    Signed-off-by: davidliu <david-khala@hotmail.com>

--- fabric-client/lib/hash.js
+++ fabric-client/lib/hash.js
@@ -17,157 +17,117 @@
 /**
  * Implement hash primitives.
  */
-var sjcl = require('sjcl');
-var sjcl_codec = require('sjcl-codec');
-var jssha = require('jssha');
+const sjcl = require('sjcl');
 
-var jsSHA3 = require('js-sha3');
-var sha3_256 = jsSHA3.sha3_256;
-var sha3_384 = jsSHA3.sha3_384;
-var shake_256 = jsSHA3.shake_256;
-var crypto = require('crypto');
+const jsSHA3 = require('js-sha3');
+const { sha3_256, sha3_384, shake_256 } = jsSHA3;
+const crypto = require('crypto');
 
-hash_sha2_256 = function (hash) {
-
-	if (hash) {
-		this._hash = hash._hash;
+class hashHex_sha2_256 extends sjcl.hash.sha256 {
+	finalize() {
+		return sjcl.codec.hex.fromBits(super.finalize());
 	}
-	else {
-		this.reset();
+	static hash(data) {
+		return (new hashHex_sha2_256()).update(String(data)).finalize();
 	}
-};
-
-hash_sha2_256.hash = function (data) {
-	return (new sjcl.hash.sha256()).update(data).finalize();
-};
-
-hash_sha2_256.prototype = {
-
-	blockSize: 512,
-
-	reset: function () {
-		this._hash = new sjcl.hash.sha256();
-		this._hash.reset();
-	},
-
-	update: function (data) {
-		this._hash.update(data);
+}
+class hashHex_sha3_256 {
+	constructor(clonedHash) {
+		this.blockSize = 1088;
+		if (clonedHash && clonedHash._hash) {
+			this._hash = clonedHash._hash;
+		}
+		else {
+			this.reset();
+		}
+	}
+	static hash(data) {
+		return sha3_256(String(data));
+	}
+	reset() {
+		this._hash = sha3_256.create();
 		return this;
-	},
-
-	finalize: function () {
-		var hash = this._hash.finalize();
+	}
+	update(data) {
+		this._hash.update(String(data));
+		return this;
+	}
+	finalize() {
+		const hash = this._hash.hex();
 		this.reset();
 		return hash;
-
 	}
-};
 
-
-var hash_sha3_256 = function (hash) {
-
-	if (hash) {
-		this._hash = hash._hash;
+}
+class hashHex_sha3_384 {
+	constructor(clonedHash) {
+		this.blockSize = 832;
+		if (clonedHash && clonedHash._hash) {
+			this._hash = clonedHash._hash;
+		}
+		else {
+			this.reset();
+		}
 	}
-	else {
-		this.reset();
+	static hash(data) {
+		return sha3_384(String(data));
 	}
-};
-
-hash_sha3_256.hash = function (data) {
-	var hashBits = sjcl.codec.hex.toBits(sha3_256(bitsToBytes(data)));
-	return hashBits;
-};
-
-hash_sha3_256.prototype = {
-
-	blockSize: 1088,
-
-	reset: function () {
-		this._hash = sha3_256.create();
-	},
-
-	update: function (data) {
-		this._hash.update(bitsToBytes(data));
-		return this;
-	},
-
-	finalize: function () {
-		var hash = this._hash.hex();
-		var hashBits = sjcl.codec.hex.toBits(hash);
-		this.reset();
-		return hashBits;
-
-	}
-};
-
-var hash_sha3_384 = function (hash) {
-
-	if (hash) {
-		this._hash = hash._hash;
-	}
-	else {
-		this.reset();
-	}
-};
-
-hash_sha3_384.hash = function (data) {
-	var hashBits = sjcl.codec.hex.toBits(sha3_384(bitsToBytes(data)));
-	return hashBits;
-};
-
-hash_sha3_384.prototype = {
-
-	blockSize: 832,
-
-	reset: function () {
+	reset() {
 		this._hash = sha3_384.create();
-	},
-
-	update: function (data) {
-		this._hash.update(bitsToBytes(data));
 		return this;
-	},
-
-	finalize: function () {
-		var hash = this._hash.hex();
-		var hashBits = sjcl.codec.hex.toBits(hash);
-		//debug('finalize hashBits:\n',hashBits)
-		this.reset();
-		return hashBits;
-
 	}
+	update(data) {
+		this._hash.update(String(data));
+		return this;
+	}
+	finalize() {
+		const hash = this._hash.hex();
+		this.reset();
+		return hash;
+	}
+}
+class hashHex_sha2_384 {
+	constructor(clonedHash) {
+		if (clonedHash && clonedHash._hash) {
+			this._hash = clonedHash._hash;
+		}
+		else {
+			this.reset();
+		}
+	}
+	static hash(data) {
+		const sha384 = crypto.createHash('sha384');
+		return sha384.update(String(data)).digest('hex');
+	}
+	reset() {
+		this._hash = crypto.createHash('sha384');
+		return this;
+	}
+	update(data) {
+		this._hash.update(String(data));
+		return this;
+	}
+	finalize() {
+		const hash = this._hash.digest('hex');
+		this.reset();
+		return hash;
+	}
+}
+exports.hash_sha3_256 = hashHex_sha3_256;
+exports.sha3_256 = (data) => {
+	return hashHex_sha3_256.hash(data);
+};
+exports.hash_sha2_256 = hashHex_sha2_256;
+exports.sha2_256 = (data) => {
+	return hashHex_sha2_256.hash(data);
+};
+exports.hash_sha3_384 = hashHex_sha3_384;
+exports.sha3_384 = (data) => {
+	return hashHex_sha3_384.hash(data);
 };
 
-/*
- * Convert from a bitArray to bytes (using SJCL's codec)
- * @param {bits} a bitArray to convert from
- * @return {bytes} the bytes converted from the bitArray
- */
-bitsToBytes = function (bits) {
-	return sjcl_codec.bytes.fromBits(bits);
+exports.hash_sha2_384 = hashHex_sha2_384;
+exports.sha2_384 = (data) => {
+	return hashHex_sha2_384.hash(data);
 };
-
-/*
- * Convert from bytes to a bitArray (using SJCL's codec)
- * @param {bytes} a bytes to convert from
- * @return {bitArray} the bitArray converted from bytes
- */
-bytesToBits = function (bytes) {
-	return sjcl_codec.bytes.toBits(bytes);
-};
-
-exports.hash_sha3_256 = hash_sha3_256;
-exports.hash_sha3_384 = hash_sha3_384;
-exports.hash_sha2_256 = hash_sha2_256;
-exports.sha2_256 = function (data) {
-	var sha256 = crypto.createHash('sha256');
-	return sha256.update(data).digest('hex');
-};
-exports.sha3_256 = sha3_256;
-exports.sha2_384 = function (data){
-	var sha384 = crypto.createHash('sha384');
-	return sha384.update(data).digest('hex');
-};
-exports.sha3_384 = sha3_384;
-exports.shake_256 = shake_256;
+exports.shake_256 = shake_256;//TODO
