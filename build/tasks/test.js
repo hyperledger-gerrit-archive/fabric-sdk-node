975caf3e2ca56a3528e123a536ec57db20e15056
zhaochy <zhaochy_2015@hotmail.com>
2018-05-02 17:19:21 +0800

    [FAB-9374] Add support for Certificates API on Node SDK
    Add support for query Certificates API on Node SDK
    
    Change-Id: I3081202bb2c590900f45f20fdb474b830d6c34df
    Signed-off-by: zhaochy <zhaochy_2015@hotmail.com>

--- build/tasks/test.js
+++ build/tasks/test.js
@@ -126,6 +126,7 @@ gulp.task('test', ['clean-up', 'lint', 'pre-test', 'compile', 'docker-ready', 'c
 		'test/integration/query.js',
 		'test/integration/fabric-ca-affiliation-service-tests.js',
 		'test/integration/fabric-ca-identity-service-tests.js',
+		'test/integration/fabric-ca-certificate-service-tests.js',
 		'test/integration/fabric-ca-services-tests.js',
 		'test/integration/client.js',
 		'test/integration/orderer-channel-tests.js',
