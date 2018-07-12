b850bf37e25d617e7bfc2447ed84b7bbf686b245
Nick Lincoln <nkl199@yahoo.co.uk>
2018-07-12 15:22:13 +0100

    establish moch tests
    This change:
    - adds a new mocha test entrance point in the main gulp file
    - changes the gulp 'test' items to ensure that the original tests are run, and then the mocha tests are run
    - adds the first mocha test for fabric-ca-client
    - removes the tape test that the above replaces
    - add the ability to run the mocha unit test at the package level
    - adds code coverage enforcement on the mocha unit tests
    - adds the required packages to dev-dependancies
    
    Change-Id: I556520c982eeef25270eee518b5f35b0da81ee06
    Signed-off-by: Nick Lincoln <nkl199@yahoo.co.uk>

--- test/unit/identity-service.js
+++ test/unit/identity-service.js
@@ -13,159 +13,165 @@ var test = _test(tape);
 var IdentityService = require('fabric-ca-client/lib/IdentityService.js');
 var FabricCAServices = require('fabric-ca-client/lib/FabricCAClientImpl');
 var FabricCAClient = FabricCAServices.FabricCAClient;
+const User = require('../../fabric-ca-client/lib/User');
 
 test('IdentityService: Test create() function', function (t) {
-	let client = new FabricCAClient({protocol: 'http', hostname: '127.0.0.1', port: 7054});
-	let identity = new IdentityService(client);
+    let client = new FabricCAClient({ protocol: 'http', hostname: '127.0.0.1', port: 7054 });
+    let identity = new IdentityService(client);
 
-	t.throws(()=> {
-		identity.create();
-	},
-	/Missing required argument "req"/,
-	'Must fail if missing request argument');
+    t.throws(() => {
+        identity.create();
+    },
+    /Missing required argument "req"/,
+    'Must fail if missing request argument');
 
-	t.throws(()=> {
-		identity.create({enrollmentID: null});
-	},
-	/Missing required parameters. "req.enrollmentID", "req.affiliation" are all required./,
-	'Must fail if missing req.enrollmentID and req.affiliation argument');
+    t.throws(() => {
+        identity.create({ enrollmentID: null });
+    },
+    /Missing required parameters. "req.enrollmentID", "req.affiliation" are all required./,
+    'Must fail if missing req.enrollmentID and req.affiliation argument');
 
-	t.throws(()=> {
-		identity.create({enrollmentID: 'dummy', affiliation: 'dummy'});
-	},
-	/Missing required argument "registrar"/,
-	'Must fail if missing registrar argument');
+    t.throws(() => {
+        identity.create({ enrollmentID: 'dummy', affiliation: 'dummy' });
+    },
+    /Missing required argument "registrar"/,
+    'Must fail if missing registrar argument');
 
-	t.throws(()=> {
-		identity.create({enrollmentID: 'dummy', affiliation: 'dummy'}, {});
-	},
-	/Argument "registrar" must be an instance of the class "User", but is found to be missing a method "getSigningIdentity/,
-	'Must fail if registrar argument is not a User object');
+    t.throws(() => {
+        identity.create({ enrollmentID: 'dummy', affiliation: 'dummy' }, {});
+    },
+    /Argument "registrar" must be an instance of the class "User"/,
+    'Must fail if registrar argument is not a User object');
 
-	t.throws(()=> {
-		identity.create({enrollmentID: 'dummy', affiliation: 'dummy'}, {getSigningIdentity: function() {return;}});
-	},
-	/Can not get signingIdentity from registrar/,
-	'Must fail if missing signingIdentity');
+    t.throws(() => {
+        let registrar = new User('bob');
+        identity.create({ enrollmentID: 'dummy', affiliation: 'dummy' }, registrar);
+    },
+    /Can not get signingIdentity from registrar/,
+    'Must fail if missing signingIdentity');
 
-	t.end();
+    t.end();
 });
 
 test('IdentityService: Test getOne() function', function (t) {
-	let client = new FabricCAClient({protocol: 'http', hostname: '127.0.0.1', port: 7054});
-	let identity = new IdentityService(client);
+    let client = new FabricCAClient({ protocol: 'http', hostname: '127.0.0.1', port: 7054 });
+    let identity = new IdentityService(client);
 
-	t.throws(()=> {
-		identity.getOne();
-	},
-	/Missing required argument "enrollmentID", or argument "enrollmentID" is not a valid string/,
-	'Must fail if missing or invalid enrollmentID argument');
+    t.throws(() => {
+        identity.getOne();
+    },
+    /Missing required argument "enrollmentID", or argument "enrollmentID" is not a valid string/,
+    'Must fail if missing or invalid enrollmentID argument');
 
-	t.throws(()=> {
-		identity.getOne('enrollmentID');
-	},
-	/Missing required argument "registrar"/,
-	'Must fail if missing registrar argument');
+    t.throws(() => {
+        identity.getOne('enrollmentID');
+    },
+    /Missing required argument "registrar"/,
+    'Must fail if missing registrar argument');
 
-	t.throws(()=> {
-		identity.getOne('enrollmentID', {});
-	},
-	/Argument "registrar" must be an instance of the class "User", but is found to be missing a method "getSigningIdentity/,
-	'Must fail if registrar argument is not a User object');
+    t.throws(() => {
+        identity.getOne('enrollmentID', {});
+    },
+    /Argument "registrar" must be an instance of the class "User"/,
+    'Must fail if registrar argument is not a User object');
 
-	t.throws(()=> {
-		identity.getOne('enrollmentID', {getSigningIdentity: function() {return;}});
-	},
-	/Can not get signingIdentity from registrar/,
-	'Must fail if missing signingIdentity');
+    t.throws(() => {
+        let registrar = new User('bob');
+        identity.getOne('enrollmentID', registrar);
+    },
+    /Can not get signingIdentity from registrar/,
+    'Must fail if missing signingIdentity');
 
-	t.end();
+    t.end();
 });
 
 test('IdentityService: Test getAll() function', function (t) {
-	let client = new FabricCAClient({protocol: 'http', hostname: '127.0.0.1', port: 7054});
-	let identity = new IdentityService(client);
+    let client = new FabricCAClient({ protocol: 'http', hostname: '127.0.0.1', port: 7054 });
+    let identity = new IdentityService(client);
 
-	t.throws(()=> {
-		identity.getAll();
-	},
-	/Missing required argument "registrar"/,
-	'Must fail if missing registrar argument');
+    t.throws(() => {
+        identity.getAll();
+    },
+    /Missing required argument "registrar"/,
+    'Must fail if missing registrar argument');
 
-	t.throws(()=> {
-		identity.getAll({});
-	},
-	/Argument "registrar" must be an instance of the class "User", but is found to be missing a method "getSigningIdentity/,
-	'Must fail if registrar argument is not a User object');
+    t.throws(() => {
+        identity.getAll({});
+    },
+    /Argument "registrar" must be an instance of the class "User"/,
+    'Must fail if registrar argument is not a User object');
 
-	t.throws(()=> {
-		identity.getAll({getSigningIdentity: function() {return;}});
-	},
-	/Can not get signingIdentity from registrar/,
-	'Must fail if missing signingIdentity');
+    t.throws(() => {
+        let registrar = new User('bob');
+        identity.getAll(registrar);
+    },
+    /Can not get signingIdentity from registrar/,
+    'Must fail if missing signingIdentity');
 
-	t.end();
+    t.end();
 });
 
 test('IdentityService: Test delete() function', function (t) {
-	let client = new FabricCAClient({protocol: 'http', hostname: '127.0.0.1', port: 7054});
-	let identity = new IdentityService(client);
+    let client = new FabricCAClient({ protocol: 'http', hostname: '127.0.0.1', port: 7054 });
+    let identity = new IdentityService(client);
 
-	t.throws(()=> {
-		identity.delete();
-	},
-	/Missing required argument "enrollmentID", or argument "enrollmentID" is not a valid string/,
-	'Must fail if missing or invalid enrollmentID argument');
+    t.throws(() => {
+        identity.delete();
+    },
+    /Missing required argument "enrollmentID", or argument "enrollmentID" is not a valid string/,
+    'Must fail if missing or invalid enrollmentID argument');
 
-	t.throws(()=> {
-		identity.delete('enrollmentID');
-	},
-	/Missing required argument "registrar"/,
-	'Must fail if missing registrar argument');
+    t.throws(() => {
+        identity.delete('enrollmentID');
+    },
+    /Missing required argument "registrar"/,
+    'Must fail if missing registrar argument');
 
-	t.throws(()=> {
-		identity.delete('enrollmentID', {});
-	},
-	/Argument "registrar" must be an instance of the class "User", but is found to be missing a method "getSigningIdentity/,
-	'Must fail if registrar argument is not a User object');
+    t.throws(() => {
+        identity.delete('enrollmentID', {});
+    },
+    /Argument "registrar" must be an instance of the class "User"/,
+    'Must fail if registrar argument is not a User object');
 
-	t.throws(()=> {
-		identity.delete('enrollmentID', {getSigningIdentity: function() {return;}});
-	},
-	/Can not get signingIdentity from registrar/,
-	'Must fail if missing signingIdentity');
+    t.throws(() => {
+        let registrar = new User('bob');
+        identity.delete('enrollmentID', registrar);
+    },
+    /Can not get signingIdentity from registrar/,
+    'Must fail if missing signingIdentity');
 
-	t.end();
+    t.end();
 });
 
 test('IdentityService: Test update() function', function (t) {
-	let client = new FabricCAClient({protocol: 'http', hostname: '127.0.0.1', port: 7054});
-	let identity = new IdentityService(client);
+    let client = new FabricCAClient({ protocol: 'http', hostname: '127.0.0.1', port: 7054 });
+    let identity = new IdentityService(client);
 
-	t.throws(()=> {
-		identity.update();
-	},
-	/Missing required argument "enrollmentID", or argument "enrollmentID" is not a valid string/,
-	'Must fail if missing or invalid enrollmentID argument');
+    t.throws(() => {
+        identity.update();
+    },
+    /Missing required argument "enrollmentID", or argument "enrollmentID" is not a valid string/,
+    'Must fail if missing or invalid enrollmentID argument');
 
-	t.throws(()=> {
-		identity.update('enrollmentID', {});
-	},
-	/Missing required argument "registrar"/,
-	'Must fail if missing registrar argument');
+    t.throws(() => {
+        identity.update('enrollmentID', {});
+    },
+    /Missing required argument "registrar"/,
+    'Must fail if missing registrar argument');
 
-	t.throws(()=> {
-		identity.update('enrollmentID', {}, {});
-	},
-	/Argument "registrar" must be an instance of the class "User", but is found to be missing a method "getSigningIdentity/,
-	'Must fail if registrar argument is not a User object');
+    t.throws(() => {
+        identity.update('enrollmentID', {}, {});
+    },
+    /Argument "registrar" must be an instance of the class "User"/,
+    'Must fail if registrar argument is not a User object');
 
-	t.throws(()=> {
-		identity.update('enrollmentID', {}, {getSigningIdentity: function() {return;}});
-	},
-	/Can not get signingIdentity from registrar/,
-	'Must fail if missing signingIdentity');
+    t.throws(() => {
+        let registrar = new User('bob');
+        identity.update('enrollmentID', {}, registrar);
+    },
+    /Can not get signingIdentity from registrar/,
+    'Must fail if missing signingIdentity');
 
-	t.end();
+    t.end();
 });
 
