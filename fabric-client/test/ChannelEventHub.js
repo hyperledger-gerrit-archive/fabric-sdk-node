1eafed1d9adbcd927346cc744826d7cceffac30c
nkl199@yahoo.co.uk <nkl199@yahoo.co.uk>
2018-08-09 14:43:54 -0400

    [FAB-11059] add unit test for policy
    - Edit Policy.js to fix where needed
    - Add Policy.js unit test with 100% coverage
    - Remove existing tape unit test
    - Add stubs for required unit tests
    
    Change-Id: I2ec6c7391110b4c0f2b1ebd7f0361feae09b4b56
    Signed-off-by: nkl199@yahoo.co.uk <nkl199@yahoo.co.uk>

---
+++ fabric-client/test/ChannelEventHub.js
@@ -0,0 +1,135 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+xdescribe('ChannelEventHub', () => {
+
+
+	describe('#constructor', () => {
+
+	});
+
+	describe('#getName', () => {
+
+	});
+
+	describe('#getPeerAddr', () => {
+
+	});
+
+	describe('#lastBlockNumber', () => {
+
+	});
+
+	describe('#_checkAllowRegistrations', () => {
+
+	});
+
+	describe('#isconnected', () => {
+
+	});
+
+	describe('#connect', () => {
+
+	});
+
+	describe('#_connect', () => {
+
+	});
+
+	describe('#close', () => {
+
+	});
+
+	describe('#_disconnect', () => {
+
+	});
+
+	describe('#_shutdown', () => {
+
+	});
+
+	describe('#_sendRegistration', () => {
+
+	});
+
+	describe('#_closeAllCallbacks', () => {
+
+	});
+
+	describe('#_checkReplay', () => {
+
+	});
+
+	describe('#_haveRegistrations', () => {
+
+	});
+
+	describe('#_checkConnection', () => {
+
+	});
+
+	describe('#checkConnection', () => {
+
+	});
+
+	describe('#registerChaincodeEvent', () => {
+
+	});
+
+	describe('#unregisterChaincodeEvent', () => {
+
+	});
+
+	describe('#registerBlockEvent', () => {
+
+	});
+
+	describe('#unregisterBlockEvent', () => {
+
+	});
+
+	describe('#registerTxEvent', () => {
+
+	});
+
+	describe('#unregisterTxEvent', () => {
+
+	});
+
+	describe('#_processBlockEvents', () => {
+
+	});
+
+	describe('#_processTxEvents', () => {
+
+	});
+
+	describe('#_callTransactionListener', () => {
+
+	});
+
+	describe('#_processChaincodeEvents', () => {
+
+	});
+
+	describe('#_callChaincodeListener', () => {
+
+	});
+
+	describe('#_checkReplayEnd', () => {
+
+	});
+});
---
+++ fabric-client/test/Client.js
@@ -0,0 +1,207 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+xdescribe('Client', () => {
+
+	describe('#constructor', () => {
+
+	});
+
+	describe('#Client.loadFromConfig', () => {
+
+	});
+
+	describe('#loadFromConfig', () => {
+
+	});
+
+	describe('#setTlsClientCertAndKey', () => {
+
+	});
+
+	describe('#addTlsClientCertAndKey', () => {
+
+	});
+
+	describe('#isDevMode', () => {
+
+	});
+
+	describe('#setDevMode', () => {
+
+	});
+
+	describe('#newChannel', () => {
+
+	});
+
+	describe('#getChannel', () => {
+
+	});
+
+	describe('#newPeer', () => {
+
+	});
+
+	describe('#getPeer', () => {
+
+	});
+
+	describe('#getPeersForOrg', () => {
+
+	});
+
+	describe('#newOrderer', () => {
+
+	});
+
+	describe('#getOrderer', () => {
+
+	});
+
+	describe('#getPeersForOrgOnChannel', () => {
+
+	});
+
+	describe('#getCertificateAuthority', () => {
+
+	});
+
+	describe('#_buildCAfromConfig', () => {
+
+	});
+
+	describe('#getClientConfig', () => {
+
+	});
+
+	describe('#getMspid', () => {
+
+	});
+
+	describe('#newTransactionID', () => {
+
+	});
+
+	describe('#extractChannelConfig', () => {
+
+	});
+
+	describe('#signChannelConfig', () => {
+
+	});
+
+	describe('#createChannel', () => {
+
+	});
+
+	describe('#updateChannel', () => {
+
+	});
+
+	describe('#_createOrUpdateChannel', () => {
+
+	});
+
+	describe('#queryChannels', () => {
+
+	});
+
+	describe('#queryInstalledChaincodes', () => {
+
+	});
+
+	describe('#installChaincode', () => {
+
+	});
+
+	describe('#initCredentialStores', () => {
+
+	});
+
+	describe('#setStateStore', () => {
+
+	});
+
+	describe('#_getSigningIdentity', () => {
+
+	});
+
+	describe('#setAdminSigningIdentity', () => {
+
+	});
+
+	describe('#_setAdminFromConfig', () => {
+
+	});
+
+	describe('#saveUserToStateStore', () => {
+
+	});
+
+	describe('#setUserContext', () => {
+
+	});
+
+	describe('#getUserContext', () => {
+
+	});
+
+	describe('#loadUserFromStateStore', () => {
+
+	});
+
+	describe('#getStateStore', () => {
+
+	});
+
+	describe('#createUser', () => {
+
+	});
+
+	describe('#_setUserFromConfig', () => {
+
+	});
+
+	describe('#getTargetPeers', () => {
+
+	});
+
+	describe('#getTargetOrderer', () => {
+
+	});
+
+	describe('#getClientCertHash', () => {
+
+	});
+
+	describe('#_checkTLScert_n_key', () => {
+
+	});
+
+	describe('#_getChaincodePackageData', () => {
+
+	});
+
+	describe('#_stringToSignature', () => {
+
+	});
+
+	describe('#_getNetworkConfig', () => {
+
+	});
+
+});
