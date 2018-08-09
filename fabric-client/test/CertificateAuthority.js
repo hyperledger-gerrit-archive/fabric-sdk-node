9d483df25e919859c495398c6d12b95898bc4461
nkl199@yahoo.co.uk <nkl199@yahoo.co.uk>
2018-08-09 14:43:54 -0400

    [FAB-11059] add unit test for policy
    - Edit Policy.js to fix where needed
    - Add Policy.js unit test with 100% coverage
    - Remove existing tape unit test
    - Add stubs for required unit tests
    
    Change-Id: I2ec6c7391110b4c0f2b1ebd7f0361feae09b4b56
    Signed-off-by: nkl199@yahoo.co.uk <nkl199@yahoo.co.uk>

---
+++ fabric-client/test/CertificateAuthority.js
@@ -0,0 +1,91 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+xdescribe('CertificateAuthority', () => {
+
+	describe('#constructor', () => {
+
+	});
+
+	describe('#getName', () => {
+
+	});
+
+	describe('#getCaName', () => {
+
+	});
+
+	describe('#getUrl', () => {
+
+	});
+
+	describe('#getConnectionOptions', () => {
+
+	});
+
+	describe('#getTlsCACerts', () => {
+
+	});
+
+	describe('#getRegistrar', () => {
+
+	});
+
+	describe('#setFabricCAServices', () => {
+
+	});
+
+	describe('#getFabricCAServices', () => {
+
+	});
+
+	describe('#register', () => {
+
+	});
+
+	describe('#enroll', () => {
+
+	});
+
+	describe('#reenroll', () => {
+
+	});
+
+	describe('#revoke', () => {
+
+	});
+
+	describe('#generateCRL', () => {
+
+	});
+
+	describe('#newCertificateService', () => {
+
+	});
+
+	describe('#newIdentityService', () => {
+
+	});
+
+	describe('#newAffiliationService', () => {
+
+	});
+
+	describe('#toString', () => {
+
+	});
+
+});
