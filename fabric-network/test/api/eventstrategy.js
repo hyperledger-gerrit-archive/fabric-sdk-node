381023ba1beb30209c7a7f3fc14fbeca6f4048da
Mark S. Lewis <mark_lewis@uk.ibm.com>
2018-08-20 17:21:27 +0100

    FABN-857, FABN-858
    FABN-857: event handler for submitTransaction()
    
    - Minor tidy-up of existing code.
    - Add default transaction event handler
    
    FABN-858: Additional selectable event handling strategies
    
    - Add multiple event handling strategies, and allow selection when
    creating a Network.
    
    Change-Id: I370a50619b659eaf399cc37cb8f48472de2b8237
    Signed-off-by: Mark S. Lewis <mark_lewis@uk.ibm.com>

---
+++ fabric-network/test/api/eventstrategy.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright 2018 IBM All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const chai = require('chai');
+chai.use(require('chai-as-promised'));
+const expect = chai.expect;
+const sinon = require('sinon');
+
+describe.skip('EventStrategy', () => {
+	describe('MSPID_SCOPE_ALLFORTX', () => {
+		it('Returns a strategy factory function', () => {
+
+		});
+	});
+});
