2a141819a7c53a36fdcd2e1214b3c4d19673022c
andrew-coleman <andrew_coleman@uk.ibm.com>
2018-09-04 13:58:47 +0100

    FABN-903 Rename fabric-network classes
    Following design review and feedback, rename the following fabric-network classes:
    Network -> Gateway
    Channel -> Network
    
    Change-Id: I3d870c34767eaf1639ec729d3c81042f105492b3
    Signed-off-by: andrew-coleman <andrew_coleman@uk.ibm.com>

--- fabric-network/lib/eventstrategies.js
+++ fabric-network/lib/eventstrategies.js
@@ -20,12 +20,12 @@ function MSPID_SCOPE_ANYFORTX(eventHubFactory, channel, mspId) {
 }
 
 function CHANNEL_SCOPE_ALLFORTX(eventHubFactory, channel, mspId) {
-	const peers = channel.getInternalChannel().getPeers();
+	const peers = channel.getChannel().getPeers();
 	return new AllForTxStrategy(eventHubFactory, peers);
 }
 
 function CHANNEL_SCOPE_ANYFORTX(eventHubFactory, channel, mspId) {
-	const peers = channel.getInternalChannel().getPeers();
+	const peers = channel.getChannel().getPeers();
 	return new AnyForTxStrategy(eventHubFactory, peers);
 }
 
