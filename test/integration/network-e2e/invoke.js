68e4a2ea87dacdf1a13a5e85913cec28b89ff4e7
Dave Kelsey <d_kelsey@uk.ibm.com>
2018-09-04 14:33:41 +0100

    FABN-903 Rename fabric-network classes
    Following design review and feedback, rename the following fabric-network classes:
    Network -> Gateway
    Channel -> Network
    
    Change-Id: Ib30aa1ce00dca573758dd3ea0a0c455519f14c17
    Signed-off-by: Dave Kelsey <d_kelsey@uk.ibm.com>

--- test/integration/network-e2e/invoke.js
+++ test/integration/network-e2e/invoke.js
@@ -11,7 +11,7 @@
 const tape = require('tape');
 const _test = require('tape-promise').default;
 const test = _test(tape);
-const {Network, InMemoryWallet, FileSystemWallet, X509WalletMixin, EventStrategies} = require('../../../fabric-network/index.js');
+const {Gateway, InMemoryWallet, FileSystemWallet, X509WalletMixin, EventStrategies} = require('../../../fabric-network/index.js');
 const fs = require('fs-extra');
 const os = require('os');
 const path = require('path');
@@ -39,15 +39,15 @@ async function tlsSetup() {
 	await inMemoryWallet.import('tlsId', X509WalletMixin.createIdentity('org1', tlsInfo.certificate, tlsInfo.key));
 }
 
-async function createContract(t, networkOptions) {
-	const network = new Network();
-	await network.initialize(JSON.parse(ccp.toString()), networkOptions);
-	t.pass('Initialized the network');
+async function createContract(t, gatewayOptions) {
+	const gateway = new Gateway();
+	await gateway.initialize(JSON.parse(ccp.toString()), gatewayOptions);
+	t.pass('Initialized the gateway');
 
-	const channel = await network.getChannel(channelName);
-	t.pass('Initialized the channel, ' + channelName);
+	const network = await gateway.getNetwork(channelName);
+	t.pass('Initialized the network, ' + channelName);
 
-	const contract = await channel.getContract(chaincodeId);
+	const contract = await network.getContract(chaincodeId);
 	t.pass('Got the contract, about to submit "move" transaction');
 
 	return contract;
@@ -250,22 +250,22 @@ test('\n\n***** Network End-to-end flow: invoke transaction to move money using 
 
 
 
-		const network = new Network();
+		const gateway = new Gateway();
 
 		const ccp = fs.readFileSync(fixtures + '/network.json');
-		await network.initialize(JSON.parse(ccp.toString()), {
+		await gateway.initialize(JSON.parse(ccp.toString()), {
 			wallet: fileSystemWallet,
 			identity: identityLabel,
 			clientTlsIdentity: 'tlsId'
 		});
 
-		t.pass('Initialized the network');
+		t.pass('Initialized the gateway');
 
-		const channel = await network.getChannel(channelName);
+		const network = await gateway.getNetwork(channelName);
 
 		t.pass('Initialized the channel, ' + channelName);
 
-		const contract = await channel.getContract(chaincodeId);
+		const contract = await network.getContract(chaincodeId);
 
 		t.pass('Got the contract, about to submit "move" transaction');
 
