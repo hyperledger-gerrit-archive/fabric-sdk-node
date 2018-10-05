d9f51dfd4d5fb189d6c9f2ebc19c4f7f457add78
nkl199@yahoo.co.uk <nkl199@yahoo.co.uk>
2018-10-05 09:36:31 +0100

    [FABN-975] Add cucumber scenario test framework
    - Framework for cucumber testing driven by CCP
    - Addition of scenarios for create/join channels based on CCP
    - Addition of scenarios for install/instantiate chaincode based on CCP
    - Addition of scenarios for network API submit/execute transaction
    - Modification of gulp test.js to include cucumber test suite
    
    Change-Id: I1abe78254db09abe28339b8f5ed58e9a55f20511
    Signed-off-by: nkl199@yahoo.co.uk <nkl199@yahoo.co.uk>

---
+++ test/features/steps/docker_steps.js
@@ -0,0 +1,30 @@
+/**
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const utils = require('../lib/utils');
+const path = require('path');
+
+module.exports = function () {
+
+	this.Given(/^I have deployed a (.+?) Fabric network/, {timeout: 240 * 1000}, async (type) => {
+		await utils.runShellCommand(undefined, 'docker kill $(docker ps -aq); docker rm $(docker ps -aq)');
+		if (type.localeCompare('non-tls') === 0) {
+			return await utils.runShellCommand(true, 'docker-compose -f ' + path.join(__dirname, '../docker-compose/docker-compose.yaml') + ' up -d');
+		} else {
+			return await utils.runShellCommand(true, 'docker-compose -f ' + path.join(__dirname, '../docker-compose/docker-compose-tls.yaml') + ' up -d');
+		}
+	});
+
+	this.Given(/^I have forcibly taken down all docker containers/, {timeout: 240 * 1000}, async () => {
+		return await utils.runShellCommand(undefined, 'docker kill $(docker ps -aq); docker rm $(docker ps -aq)');
+	});
+
+
+	this.Given(/^I have deleted all dev images/, {timeout: 240 * 1000}, async () => {
+		return await utils.runShellCommand(undefined, 'docker rmi $(docker images dev-* -q)');
+	});
+
+};
