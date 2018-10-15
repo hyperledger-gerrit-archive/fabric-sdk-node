004ba77dfc118a5e2b30ee655461d1a13ae587af
rameshthoomu <rameshbabu.thoomu@gmail.com>
2018-10-15 14:57:11 -0400

    FABCI-22 Pipeline job configuration
    Update npm node module and docs publish script with the
    latest changes
    
    Change-Id: Ibbb0af25422faae10ce4ae19f9e1500aeda8e6f0
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- scripts/Jenkins_Scripts/CI_Script.sh
+++ scripts/Jenkins_Scripts/CI_Script.sh
@@ -7,14 +7,6 @@
 
 # exit on first error
 
-export BASE_FOLDER=$WORKSPACE/gopath/src/github.com/hyperledger
-# Modify this when change the image tag
-export STABLE_TAG=1.3.0-stable
-export NEXUS_URL=nexus3.hyperledger.org:10001
-export ORG_NAME="hyperledger/fabric"
-# Set this in GOPATH
-export NODE_VER=8.9.4 # Default nodejs version
-
 # Fetch baseimage version
 curl -L https://raw.githubusercontent.com/hyperledger/fabric/master/Makefile > Makefile
 export BASE_IMAGE_VER=`cat Makefile | grep BASEIMAGE_RELEASE= | cut -d "=" -f2`
@@ -46,9 +38,6 @@ Parse_Arguments() {
                             ;;
                       --pull_Fabric_Images)
                             pull_Fabric_Images
-                            ;;
-                      --pull_Fabric_CA_Image)
-                            pull_Fabric_CA_Image
                             ;;
                       --clean_Environment)
                             clean_Environment
@@ -150,41 +139,22 @@ pull_Thirdparty_Images() {
 }
 # pull fabric images from nexus
 pull_Fabric_Images() {
-            for IMAGES in peer orderer; do
+            for IMAGES in peer orderer ca; do
                  echo "-----------> pull $IMAGES image"
                  echo
-                 docker pull $NEXUS_URL/$ORG_NAME-$IMAGES:$STABLE_TAG
-                 docker tag $NEXUS_URL/$ORG_NAME-$IMAGES:$STABLE_TAG $ORG_NAME-$IMAGES
-                 docker tag $NEXUS_URL/$ORG_NAME-$IMAGES:$STABLE_TAG $ORG_NAME-$IMAGES:$STABLE_TAG
-                 docker rmi -f $NEXUS_URL/$ORG_NAME-$IMAGES:$STABLE_TAG
+                 docker pull $NEXUS_REPO_URL/$ORG_NAME-$IMAGES:$STABLE_TAG
+                 docker tag $NEXUS_REPO_URL/$ORG_NAME-$IMAGES:$STABLE_TAG $ORG_NAME-$IMAGES
+                 docker tag $NEXUS_REPO_URL/$ORG_NAME-$IMAGES:$STABLE_TAG $ORG_NAME-$IMAGES:$STABLE_TAG
+                 docker rmi -f $NEXUS_REPO_URL/$ORG_NAME-$IMAGES:$STABLE_TAG
             done
                  echo
                  docker images | grep hyperledger/fabric
 }
-# pull fabric-ca images from nexus
-pull_Fabric_CA_Image() {
-        echo
-            for IMAGES in ca; do
-                 echo "-----------> pull $IMAGES image"
-                 echo
-                 docker pull $NEXUS_URL/$ORG_NAME-$IMAGES:$STABLE_TAG
-                 docker tag $NEXUS_URL/$ORG_NAME-$IMAGES:$STABLE_TAG $ORG_NAME-$IMAGES
-	         docker tag $NEXUS_URL/$ORG_NAME-$IMAGES:$STABLE_TAG $ORG_NAME-$IMAGES:$STABLE_TAG
-                 docker rmi -f $NEXUS_URL/$ORG_NAME-$IMAGES:$STABLE_TAG
-            done
-                 echo
-                 docker images | grep hyperledger/fabric-ca
-}
+
 # run sdk e2e tests
 sdk_E2e_Tests() {
         echo
         echo "-----------> Execute NODE SDK E2E Tests"
-        cd ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-sdk-node/test/fixtures || exit
-        docker-compose up >> dockerlogfile.log 2>&1 &
-        sleep 30
-        echo "---------> LIST DOCKER CONTAINERS"
-        docker ps -a
-
         cd ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-sdk-node || exit
         # Install nvm to install multi node versions
         wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
