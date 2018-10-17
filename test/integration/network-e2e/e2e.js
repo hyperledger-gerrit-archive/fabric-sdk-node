896905b6ee700786174738c07146760bbc9d5c41
Liam Grace <liamgrace.896@gmail.com>
2018-10-17 10:36:33 +0100

    [FABN-863] Added HSM wallet mixin
    - Unit tests using mocha at 100%
    - Integration tests
    - README updates to move HSM support out of experimental
    - Change to include HSM unit tests in test suite by defualt
    
    Change-Id: I4b5c2264f1386fcc85f8566fdabb379a99ac0c8f
    Signed-off-by: Liam Grace <liamgrace.896@gmail.com>

--- test/integration/network-e2e/e2e.js
+++ test/integration/network-e2e/e2e.js
@@ -7,5 +7,3 @@ require('../e2e/create-channel.js');
 require('../e2e/join-channel.js');
 require('./install-chaincode.js');
 require('./instantiate-chaincode.js');
-require('./invoke.js');
-require('./query.js');
