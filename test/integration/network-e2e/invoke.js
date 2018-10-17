33f45bcbb5ba5c806d893e7c9aa4777726dffb00
Liam Grace <liamgrace.896@gmail.com>
2018-10-17 10:36:33 +0100

    [FABN-863] Added HSM wallet mixin
    - Unit tests using mocha at 100%
    - Integration tests
    - README updates to move HSM support out of experimental
    - Change to include HSM unit tests in test suite by defualt
    
    Change-Id: I4b5c2264f1386fcc85f8566fdabb379a99ac0c8f
    Signed-off-by: Liam Grace <liamgrace.896@gmail.com>

--- test/integration/network-e2e/invoke.js
+++ test/integration/network-e2e/invoke.js
@@ -25,7 +25,6 @@ const testUtils = require('../../unit/util');
 const channelName = testUtils.NETWORK_END2END.channel;
 const chaincodeId = testUtils.NETWORK_END2END.chaincodeId;
 
-
 const fixtures = process.cwd() + '/test/fixtures';
 const credPath = fixtures + '/channel/crypto-config/peerOrganizations/org1.example.com/users/User1@org1.example.com';
 const cert = fs.readFileSync(credPath + '/signcerts/User1@org1.example.com-cert.pem').toString();
@@ -62,7 +61,7 @@ async function getFirstEventHubForOrg(gateway, orgMSP) {
 	return channel.getChannelEventHub(orgPeer.getName());
 }
 
-test('\n\n***** Network End-to-end flow: import identity into wallet *****\n\n', async (t) => {
+test('\n\n***** network end-to-end flow: import identity into wallet *****\n\n', async (t) => {
 	await inMemoryIdentitySetup();
 	const exists = await inMemoryWallet.exists('User1@org1.example.com');
 	if(exists) {
@@ -72,6 +71,7 @@ test('\n\n***** Network End-to-end flow: import identity into wallet *****\n\n',
 	}
 	t.end();
 });
+
 
 test('\n\n***** Network End-to-end flow: invoke transaction to move money using in memory wallet and default event strategy *****\n\n', async (t) => {
 	const gateway = new Gateway();
