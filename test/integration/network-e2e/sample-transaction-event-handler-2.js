c34b41c3f54f56c7556e2d81c89068463843a6e7
Mark S. Lewis <mark_lewis@uk.ibm.com>
2018-10-19 09:38:15 +0100

    FABN-865: Simplify sample plug-in event handler
    Register and connect event hubs in parallel.
    
    Change-Id: I9e402ef14d32f931ff2f62d60e94d2f1d0c01287
    Signed-off-by: Mark S. Lewis <mark_lewis@uk.ibm.com>

--- test/integration/network-e2e/sample-transaction-event-handler-2.js
+++ test/integration/network-e2e/sample-transaction-event-handler-2.js
@@ -6,153 +6,75 @@
 
 'use strict';
 
-//--- Plug-in event handler sample where we provide an event hub factory to help user obtain and connect event hubs
+//--- Plug-in event handler sample where we provide both an event handler factory and our transaction event handler.
+//--- The user just implements a plug-in strategy for the event handler, which exposes an additional API.
 
-/**
- * Handler that listens for commit events for a specific transaction from a set of event hubs.
- * A new instance of this class should be created to handle each transaction as it maintains state
- * related to events for a given transaction.
- * @class
- */
-class SampleTransactionEventHandler {
+const TransactionEventHandler = require('fabric-network/lib/impl/event/transactioneventhandler');
+
+class SampleEventHandlerStrategy {
 	/**
 	 * Constructor.
-	 * @param {String} transactionId Transaction ID for which events will be received.
-	 * @param {Promise.ChannelEventHub[]} eventHubsPromise Connected event hubs from which events will be received.
-	 * @param {Object} [options] Additional configuration options.
-	 * @param {Number} [options.commitTimeout] Time in seconds to wait for commit events to be reveived.
+	 * @param {Promise.ChannelEventHub[]} eventHubsPromise Promise to event hubs for which to process events.
 	 */
-	constructor(transactionId, eventHubsPromise, options) {
-		this.transactionId = transactionId;
+	constructor(eventHubsPromise) {
 		this.eventHubsPromise = eventHubsPromise;
-
-		const defaultOptions = {
-			commitTimeout: 120 // 2 minutes
-		};
-		this.options = Object.assign(defaultOptions, options);
-
-		this.eventHubs = [];
-
-		this.notificationPromise = new Promise((resolve, reject) => {
-			this._txResolve = resolve;
-			this._txReject = reject;
-		});
 	}
 
 	/**
-	 * Called to initiate listening for transaction events.
+	 * Called by event handler to obtain the event hubs to which it should listen. Gives an opportunity for
+	 * the strategy to store information on the events it expects to receive for later use in event handling.
 	 * @async
-	 * @throws {Error} if not in a state where the handling strategy can be satified and the transaction should
-	 * be aborted. For example, if insufficient event hubs could be connected.
+	 * @returns {ChannelEventHubs[]} connected event hubs.
+	 * @throws {Error} if the connected event hubs do not satisfy the strategy.
 	 */
-	async startListening() {
-		this.eventHubs = await this._getConnectedEventHubs();
-		if (this.eventHubs.length > 0) {
-			this.eventCounts = {
-				expected: this.eventHubs.length,
-				received: 0
-			};
-			this._registerTxEventListeners();
-			this._setListenTimeout();
-		} else {
-			// Assume success if unable to listen for events
-			this._txResolve();
-		}
-	}
-
-	/**
-     * Wait until enough events have been received from the event hubs to satisfy the event handling strategy.
-     * @async
-	 * @throws {Error} if the transaction commit is not successful within the timeout period.
-     */
-	async waitForEvents() {
-		await this.notificationPromise;
-	}
-
-	/**
-     * Cancel listening for events.
-     */
-	cancelListening() {
-		clearTimeout(this.timeoutHandler);
-		this.eventHubs.forEach((eventHub) => {
-			eventHub.unregisterTxEvent(this.transactionId);
-		});
-	}
-
-	async _getConnectedEventHubs() {
+	async getConnectedEventHubs() {
 		const eventHubs = await this.eventHubsPromise;
-		return eventHubs.filter((eventHub) => eventHub.isconnected());
+		const connectedEventHubs = eventHubs.filter((eventHub) => eventHub.isconnected());
+
+		this.eventCounts = {
+			expected: connectedEventHubs.length,
+			received: 0
+		};
+
+		return connectedEventHubs;
 	}
 
-	_registerTxEventListeners() {
-		for (const eventHub of this.eventHubs) {
-			eventHub.registerTxEvent(this.transactionId,
-				(txId, code) => this._onEvent(eventHub, txId, code),
-				(err) => this._onError(eventHub, err));
-		}
+	/**
+	 * Called when an event is received.
+	 * @param {Function} successFn Callback function to invoke if this event satisfies the strategy.
+	 * @param {Function} failFn Callback function to invoke if this event fails the strategy.
+	 */
+	eventReceived(successFn, failFn) {
+		this._responseReceived(successFn, failFn);
 	}
 
-	_onEvent(eventHub, txId, code) {
-		eventHub.unregisterTxEvent(this.transactionId);
-		if (code !== 'VALID') {
-			// Peer has rejected the transaction so stop listening with a failure
-			const message = `Peer ${eventHub.getPeerAddr()} has rejected transaction ${txId} with code ${code}`;
-			this._fail(new Error(message));
-		} else {
-			// --------------------------------------------------------------
-			// Handle processing of successful transaction commit events here
-			// --------------------------------------------------------------
-			this._responseReceived();
-		}
-	}
-
-	_onError(eventHub, err) { // eslint-disable-line no-unused-vars
-		eventHub.unregisterTxEvent(this.transactionId);
-		// --------------------------------------------------
-		// Handle processing of event hub disconnections here
-		// --------------------------------------------------
-		this._responseReceived();
+	/**
+	 * Called when an error is received.
+	 * @param {Function} successFn Callback function to invoke if this error satisfies the strategy.
+	 * @param {Function} failFn Callback function to invoke if this error fails the strategy.
+	 */
+	errorReceived(successFn, failFn) {
+		this._responseReceived(successFn, failFn);
 	}
 
 	/**
 	 * Simple event handling logic that is satisfied once all of the event hubs have either responded with valid
 	 * events or disconnected.
 	 */
-	_responseReceived() {
+	_responseReceived(successFn, failFn) { // eslint-disable-line no-unused-vars
 		this.eventCounts.received++;
 		if (this.eventCounts.received === this.eventCounts.expected) {
-			this._success();
+			successFn();
 		}
-	}
-
-	_setListenTimeout() {
-		if (this.options.commitTimeout > 0) {
-			return;
-		}
-
-		this.timeoutHandler = setTimeout(() => {
-			this._fail(new Error(`Timeout waiting for commit events for transaction ID ${this.transactionId}`));
-		}, this.options.commitTimeout * 1000);
-	}
-
-	_fail(error) {
-		this.cancelListening();
-		this._txReject(error);
-	}
-
-	_success() {
-		this.cancelListening();
-		this._txResolve();
 	}
 }
 
 function createTransactionEventHandler(transactionId, network) {
-	const channel = network.getChannel();
-	const peers = channel.getPeersForOrg();
+	const peers = network.getChannel().getPeersForOrg();
 	const eventHubsPromise = network.getEventHubFactory().getEventHubs(peers);
-	return new SampleTransactionEventHandler(transactionId, eventHubsPromise);
+	const strategy = new SampleEventHandlerStrategy(eventHubsPromise);
+	const options = { commitTimeout: 120 };
+	return new TransactionEventHandler(transactionId, strategy, options);
 }
 
-module.exports = {
-	sampleEventStrategy: createTransactionEventHandler
-};
+module.exports = createTransactionEventHandler;
