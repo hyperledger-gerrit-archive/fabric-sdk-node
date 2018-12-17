be49cfebeb6c0df93ff6c14808c735fefc0591bb
rameshthoomu <rameshbabu.thoomu@gmail.com>
2018-12-17 15:15:45 -0500

    FABN-1082 Update NPM module script
    This change updates the npm module script to assign a value
    to the Unstable Version when the unstable tag is not available
    in the npm registry.
    
    Also this change removes the hardcoded VERSION to get the stable images
    from nexus, instead this always get the $ARCH-latest images from
    nexus repo
    
    Change-Id: I90ad44debf227fdba31b00ea64fd382e3955e72d
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- scripts/Jenkins_Scripts/Publish_NPM_Modules.sh
+++ scripts/Jenkins_Scripts/Publish_NPM_Modules.sh
@@ -7,67 +7,69 @@
 
 npmPublish() {
 
- if [[ "$CURRENT_TAG" = *"skip"* ]]; then
+if [[ "$CURRENT_TAG" = *"skip"* ]]; then
      echo "----> Don't publish $1 npm modules on skip tag"
- elif [[ "$CURRENT_TAG" = *"unstable"* ]]; then
-      echo
-      UNSTABLE_VER=$(npm dist-tags ls "$1" | awk "/$CURRENT_TAG"":"/'{
-      ver=$NF
-      sub(/.*\./,"",rel)
-      sub(/\.[[:digit:]]+$/,"",ver)
-      print ver}')
-      echo "======> UNSTABLE VERSION:" $UNSTABLE_VER
+elif [[ "$CURRENT_TAG" = *"unstable"* ]]; then
 
-      UNSTABLE_INCREMENT=$(npm dist-tags ls "$1" | awk "/$CURRENT_TAG"":"/'{
-      ver=$NF
-      rel=$NF
-      sub(/.*\./,"",rel)
-      sub(/\.[[:digit:]]+$/,"",ver)
-      print ver"."rel+1}')
-      echo "======> Incremented UNSTABLE VERSION:" $UNSTABLE_INCREMENT
+     UNSTABLE_INCREMENT=$(npm dist-tags ls "$1" | awk "/$CURRENT_TAG"":"/'{
+     ver=$NF
+     rel=$NF
+     sub(/.*\./,"",rel)
+     sub(/\.[[:digit:]]+$/,"",ver)
+     print ver"."rel+1}')
 
-      # Get last digit of the unstable version of $CURRENT_TAG
-      UNSTABLE_INCREMENT=$(echo $UNSTABLE_INCREMENT| rev | cut -d '.' -f 1 | rev)
-      echo "======> UNSTABLE_INCREMENT:" $UNSTABLE_INCREMENT
+     if [[ $UNSTABLE_VER = "" ]]; then
+        echo -e "\033[34m  ----> unstable ver is blank" "\033[0m"
+        UNSTABLE_INCREMENT=1
+     else
+        # Get last digit of the unstable version of $CURRENT_TAG
+        UNSTABLE_INCREMENT=$(echo $UNSTABLE_INCREMENT| rev | cut -d '.' -f 1 | rev)
+        echo "\033[32m======> UNSTABLE_INCREMENT:" $UNSTABLE_INCREMENT "\033[0m"
+     fi
+     echo -e "\033[32m======> UNSTABLE_INCREMENT:" $UNSTABLE_INCREMENT "\033[0m"
 
-      # Append last digit with the package.json version
-      export UNSTABLE_INCREMENT_VERSION=$RELEASE_VERSION.$UNSTABLE_INCREMENT
-      echo "======> UNSTABLE_INCREMENT_VERSION:" $UNSTABLE_INCREMENT_VERSION
+     # Get last digit of the unstable version of $CURRENT_TAG
+     UNSTABLE_INCREMENT=$(echo $UNSTABLE_INCREMENT| rev | cut -d '.' -f 1 | rev)
+     echo "\033[32m======> UNSTABLE_INCREMENT:" $UNSTABLE_INCREMENT "\033[0m"
 
-      if [ "$1" = "fabric-network" ]; then
+     # Append last digit with the package.json version
+     export UNSTABLE_INCREMENT_VERSION=$RELEASE_VERSION.$UNSTABLE_INCREMENT
+     echo "\033[32m======> UNSTABLE_INCREMENT_VERSION:" $UNSTABLE_INCREMENT_VERSION "\033[0m"
+
+     if [ "$1" = "fabric-network" ]; then
           sed -i 's/\(.*\"fabric-client\"\: \"\)\(.*\)/\1'$CURRENT_TAG\"\,'/' package.json
           sed -i 's/\(.*\"fabric-ca-client\"\: \"\)\(.*\)/\1'$CURRENT_TAG\"\,'/' package.json
-      fi
+     fi
 
-      # Replace existing version with $UNSTABLE_INCREMENT_VERSION
-      sed -i 's/\(.*\"version\"\: \"\)\(.*\)/\1'$UNSTABLE_INCREMENT_VERSION\"\,'/' package.json
-      # Show Version after modify the package.json with latest version to publish
-      grep '"version":' package.json | cut -d\" -f4
-      # Publish unstable versions to npm registry
-      npm publish --tag $CURRENT_TAG
-      if [ $? != 0 ]; then
-           echo -e "\033[31m FAILED to Publish $CURRENT_TAG of $1 npm module" "\033[0m"
-           exit 1
-      fi
-      echo -e "\033[32m ========> PUBLISHED $CURRENT_TAG tag of $1 npm module SUCCESSFULLY" "\033[0m"
+     # Replace existing version with $UNSTABLE_INCREMENT_VERSION
+     sed -i 's/\(.*\"version\"\: \"\)\(.*\)/\1'$UNSTABLE_INCREMENT_VERSION\"\,'/' package.json
+     # Show Version after modify the package.json with latest version to publish
+     grep '"version":' package.json | cut -d\" -f4
+     # Publish unstable versions to npm registry
+     npm publish --tag $CURRENT_TAG
+     if [ $? != 0 ]; then
+          echo -e "\033[31m FAILED to Publish $CURRENT_TAG of $1 npm module" "\033[0m"
+          exit 1
+     fi
+     echo -e "\033[32m ========> PUBLISHED $CURRENT_TAG tag of $1 npm module SUCCESSFULLY" "\033[0m"
 
- else
-      # Publish node modules on latest tag
-      echo -e "\033[32m ========> PUBLISH $RELEASE_VERSION" "\033[0m"
+else
+     # Publish node modules on the current tag
+     echo -e "\033[32m ========> PUBLISH $RELEASE_VERSION" "\033[0m"
 
-      if [ "$1" = "fabric-network" ]; then
+     if [ "$1" = "fabric-network" ]; then
           sed -i 's/\(.*\"fabric-client\"\: \"\)\(.*\)/\1'$CURRENT_TAG\"\,'/' package.json
           sed -i 's/\(.*\"fabric-ca-client\"\: \"\)\(.*\)/\1'$CURRENT_TAG\"\,'/' package.json
-      fi
+     fi
 
-      npm publish --tag $CURRENT_TAG
+     npm publish --tag $CURRENT_TAG
 
-      if [ $? != 0 ]; then
-           echo -e "\033[31m FAILED TO PUBLISH $CURRENT_TAG of $1 npm module" "\033[0m"
-           exit 1
-      fi
-      echo -e "\033[32m ========> PUBLISHED $CURRENT_TAG tag of $1 npm module SUCCESSFULLY" "\033[0m"
- fi
+     if [ $? != 0 ]; then
+          echo -e "\033[31m FAILED TO PUBLISH $CURRENT_TAG of $1 npm module" "\033[0m"
+          exit 1
+     fi
+     echo -e "\033[32m ========> PUBLISHED $CURRENT_TAG tag of $1 npm module SUCCESSFULLY" "\033[0m"
+fi
 }
 
 versions() {
@@ -92,9 +94,9 @@ npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN
 # Add or delete modules from here..
 for modules in fabric-network fabric-common fabric-ca-client fabric-client; do
      if [ -d "$modules" ]; then
-           echo -e "\033[32m Publishing $modules" "\033[0m"
-           cd $modules
-           versions
+          echo -e "\033[32m Publishing $modules" "\033[0m"
+          cd $modules
+          versions
           npmPublish $modules
           cd -
      fi
