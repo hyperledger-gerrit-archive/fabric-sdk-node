1b9aa902fbbef02ff75d210bf00e82dc48ceff95
Liam Grace <liamgrace.896@gmail.com>
2019-02-21 14:13:55 +0000

    [FABN-599] Added functional test framework
    Change-Id: Ie2550ba4932b4e2be7b7c0185802b3e467ff0c49
    Signed-off-by: Liam Grace <liamgrace.896@gmail.com>

---
+++ test/functional/eventhandling/chaincodeevents.js
@@ -0,0 +1,16 @@
+/*
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+*/
+
+'use strict';
+
+const chai = require('chai');
+const {expect} = chai;
+
+describe('Chaincode Events', () => {
+	it('initial test designed to pass', () => {
+		expect(true).to.be.true;
+	});
+});
