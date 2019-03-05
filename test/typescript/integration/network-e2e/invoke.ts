243c3d193612ca4dea85676caadf4ca54ef964b5
nkl199@yahoo.co.uk <nkl199@yahoo.co.uk>
2019-03-05 15:08:13 +0000

    [FABN-1169] Backport certifcate fix from master
    - Backport fix from master
    - Enable mutual TLS (client authetication)
    
    Change-Id: Id2fab00875f643e7734458158f3a5dddcf6f5360
    Signed-off-by: nkl199@yahoo.co.uk <nkl199@yahoo.co.uk>

--- test/typescript/integration/network-e2e/invoke.ts
+++ test/typescript/integration/network-e2e/invoke.ts
@@ -43,7 +43,7 @@ const channelName: string = testUtils.NETWORK_END2END.channel;
 const chaincodeId: string = testUtils.NETWORK_END2END.chaincodeId;
 
 const fixtures = process.cwd() + '/test/fixtures';
-const credPath = fixtures + '/channel/crypto-config/peerOrganizations/org1.example.com/users/User1@org1.example.com';
+const credPath = fixtures + '/crypto-material/crypto-config/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp';
 const certificatePem: string = fs.readFileSync(credPath + '/signcerts/User1@org1.example.com-cert.pem').toString();
 const privateKeyPem: string = fs.readFileSync(credPath + '/keystore/e4af7f90fa89b3e63116da5d278855cfb11e048397261844db89244549918731_sk').toString();
 const inMemoryWallet: Wallet = new InMemoryWallet();
