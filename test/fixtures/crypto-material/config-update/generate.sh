b997d32e6f9aed91ed1f437f2997dd3e43592c9d
nkl199@yahoo.co.uk <nkl199@yahoo.co.uk>
2019-03-07 11:09:51 +0000

    [FABN-1169] auto-gen test certs in build
    - Add gulp scripts to automatically download fabric binaries and generate certs
    - Add generate certs step to Jenkins build scripts
    - Remove git-cached certs from repository
    - Add to ReadMe files the required initial steps to get certs locally
    
    Change-Id: Id13634c6bcc1bb8daab531bd8185da58cb5093f8
    Signed-off-by: nkl199@yahoo.co.uk <nkl199@yahoo.co.uk>

--- test/fixtures/crypto-material/config-update/generate.sh
+++ test/fixtures/crypto-material/config-update/generate.sh
@@ -2,8 +2,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-echo 'Generating new channel update tx....'
-export FABRIC_CFG_PATH=$PWD
-configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ../channel-config/mychannel-org1anchor.tx -channelID mychannel -asOrg Org1MSP
-configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ../channel-config/discovery_anchor.tx -channelID discovery -asOrg Org1MSP
+BASEDIR=$(dirname $(realpath $0))
+echo "Creating new channel update tx blocks from within directory ${BASEDIR}"
+export FABRIC_CFG_PATH=$BASEDIR
+configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ${BASEDIR}/../channel-config/mychannel-org1anchor.tx -channelID mychannel -asOrg Org1MSP
+configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ${BASEDIR}/../channel-config/discovery_anchor.tx -channelID discovery -asOrg Org1MSP
 
