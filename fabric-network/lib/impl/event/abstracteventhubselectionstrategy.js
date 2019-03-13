053951726c48ee24e57c66e4c4d14b42732da0f5
Liam Grace <liamgrace.896@gmail.com>
2019-03-13 10:41:54 +0000

    [FABN-1100] Event handling abstractions <DO NOT MERGE>
    - Handle commit, contract and block events
    - Checkpoint each event listener to allow easy event catch up
    - New event hub manager to centralise all event hubs used by fabric-network
    - Automatic reconnect to an event hub when a registered listener loses connection
    
    Change-Id: I93f3df089e5393fbaf90592528463f1ee5d79b1d
    Signed-off-by: Liam Grace <liamgrace.896@gmail.com>

---
+++ fabric-network/lib/impl/event/abstracteventhubselectionstrategy.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2018 IBM All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+/**
+ * An abstract selection strategy that provides an interface for other selection
+ * strategies
+ */
+class AbstractEventHubSelectionStrategy {
+	/**
+	 * Gets the next peer
+	 * @returns {Peer}
+	 */
+	getNextPeer() {
+		throw new Error('Abstract method called.');
+	}
+
+	/**
+	 * Updates the availablility of the peer
+	 * @param {Peer} deadPeer The peer that went down
+	 */
+	updateEventHubAvailability(deadPeer) {
+		return;
+	}
+}
+
+module.exports = AbstractEventHubSelectionStrategy;
