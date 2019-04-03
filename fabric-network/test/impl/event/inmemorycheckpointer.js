55657876da4dd87bb9dbeaf998bb7eea82b96d3a
Liam Grace <liamgrace.896@gmail.com>
2019-04-03 12:29:50 +0100

    [FABN-1100] Tidy start block and event hub management
    Change-Id: I44b3683283fa2d13404ee15d109af7fc6dd2159a
    Signed-off-by: Liam Grace <liamgrace.896@gmail.com>

---
+++ fabric-network/test/impl/event/inmemorycheckpointer.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const BaseCheckpointer = require('./../../../lib/impl/event/basecheckpointer');
+
+class InMemoryCheckpointer extends BaseCheckpointer {
+	constructor() {
+		super();
+		this.checkpoint = {};
+	}
+
+	async load() {
+		return this.checkpoint;
+	}
+
+	async save(transactionId, blockNumber) {
+		if (Number(blockNumber) !== Number(this.checkpoint.blockNumber)) {
+			this.checkpoint = {blockNumber: Number(blockNumber), transactionIds: [transactionId]};
+		} else {
+			this.checkpoint.transactionIds.push(transactionId);
+		}
+	}
+}
+
+module.exports = InMemoryCheckpointer;
