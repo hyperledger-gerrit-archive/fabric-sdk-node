94606b1d740acb77a9c42629b84720287c12b4f3
Bret Harrison <beharrison@nc.rr.com>
2019-06-04 09:08:30 -0400

    FABN-1347 NodeSDK fabric-base initial
    Initial files added to new module fabric-base.
    Some unit test and an integration test
    showing the complete flow of an endorsement, commit with
    monitoring for an event, query and discovery calls.
    
    Note that the channel class is not the focal point
    of the flow, the new class "Proposal" is the object that
    is used to build up the proposal, sign it, and then
    send it. Then again to build up the commit submission,
    using the results of the endorsement, sign it, then send
    it to the orderer. The "Client" class is still the central
    location for client side shared connection settings like the
    client side mutual TLS. A new class "TransactionContext" is
    applied at each building of a proposal, avoiding the client
    class having user state and a state or crypto store.
    New ChannelDiscovery class added to be the focal point of discovery
    calls to the peer for a channel. This class will build
    and sign the discovery request. This class will
    decode the discover configurations and
    endorsement plans. It will provide peers and orderers to
    the channel along with the plan for the endorsement hsndler,
    query handler, and commit handler. The dicovered peers and
    organizations will be the source endpoint for event monitoring.
    
    Change-Id: If7dc29aaab2697d4979b36f209a23bfc43588d56
    Signed-off-by: Bret Harrison <beharrison@nc.rr.com>
    Signed-off-by: david <david-khala@hotmail.com>

---
+++ fabric-base/test/Proposal.js
@@ -0,0 +1,130 @@
+/**
+ * Copyright 2019 IBM All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const rewire = require('rewire');
+
+const Proposal_Rewired = rewire('../lib/Proposal');
+
+const Channel = require('../lib/Channel');
+const Chaincode = require('../lib/Chaincode');
+const Proposal = require('../lib/Proposal');
+const TransactionContext = require('../lib/TransactionContext');
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const sinon = require('sinon');
+const should = chai.should();
+const expect = chai.expect;
+chai.use(chaiAsPromised);
+
+function propertiesToBeInstanceOf(obj, properties, clazz) {
+	properties.forEach((prop) => {
+		if (obj.hasOwnProperty(prop)) {
+			obj[prop].should.be.instanceof(clazz);
+		} else {
+			should.fail();
+		}
+	});
+}
+
+describe('Proposal', () => {
+	let sandbox;
+	let revert;
+	let FakeLogger;
+
+	let chaincode;
+	let channel;
+	let proposal;
+	let identity;
+
+	beforeEach(() => {
+		revert = [];
+		sandbox = sinon.createSandbox();
+
+		FakeLogger = {
+			debug: () => { },
+			error: () => { }
+		};
+		sandbox.stub(FakeLogger);
+		revert.push(Proposal_Rewired.__set__('logger', FakeLogger));
+
+		chaincode = new Chaincode('mychaincode', 'v1');
+		channel = new Channel('mychannel');
+		proposal = new Proposal_Rewired(chaincode, channel);
+		identity = 'identity';
+	});
+
+	afterEach(() => {
+		if (revert.length) {
+			revert.forEach(Function.prototype.call, Function.prototype.call);
+		}
+		sandbox.restore();
+	});
+
+	describe('#constructor', () => {
+		it('should require a chaincode', () => {
+			(() => {
+				new Proposal();
+			}).should.throw('Missing chaincode parameter');
+		});
+
+		it('should require a channel', () => {
+			(() => {
+				new Proposal({});
+			}).should.throw('Missing channel parameter');
+		});
+
+		it('should create an instance and define the correct properties', () => {
+			const proposal_real = new Proposal(chaincode, channel);
+			propertiesToBeInstanceOf(proposal_real, ['chaincode'], Chaincode);
+			propertiesToBeInstanceOf(proposal_real, ['channel'], Channel);
+		});
+	});
+
+	describe('#toString', () => {
+		it('should get the object contents in string form', () => {
+			const value = proposal.toString();
+			should.equal(value, 'Proposal: {chaincode: mychaincode, channel: mychannel, fcn: invoke}');
+		});
+	});
+
+	describe('#getSignedProposalEnvelope', () => {
+		it('should be able to get a signed Proposal envelope', () => {
+
+			proposal.getSignedProposalEnvelope();
+			should.equal(proposal.fcn, 'function');
+		});
+	});
+
+	describe('#setFunctionArguments', () => {
+		it('should be able to set a function arguments', () => {
+			proposal.setFunctionArguments(['arg1', 'arg2']);
+			expect(proposal.args).to.deep.equal(['arg1', 'arg2']);
+
+		});
+	});
+
+	describe('#setTransactionContext', () => {
+		it('should be able to set a txContext', () => {
+			const txContext = new TransactionContext(identity);
+			proposal.setTransactionContext(txContext);
+			expect(proposal.txContext).to.deep.equal(txContext);
+
+		});
+	});
+
+	describe('#setTransientMap', () => {
+		it('should be able to set a transient map', () => {
+			const transientMap = new Map();
+			transientMap.set('key1', 'value1');
+			transientMap.set('key2', 'value2');
+			proposal.setTransientMap(transientMap);
+			expect(proposal.transientMap).to.deep.equal(transientMap);
+		});
+	});
+});
