b1d043f6aa5e2c0a2f16432c15b4c945680578fe
Bret Harrison <beharrison@nc.rr.com>
2019-06-04 09:08:30 -0400

    FABN-1347 NodeSDK fabric-base initial
    Initial files added to new module fabric-base.
    Some unit test and an integration test
    showing the complete flow of an endorsement, commit with
    monitoring for an event, query and discovery calls.
    
    Note that the channel class is not the focal point
    of the flow, the new class "Proposal" is the object that
    is used to build up the proposal, sign it, and then
    send it. Then again to build up the commit submission,
    using the results of the endorsement, sign it, then send
    it to the orderer. The "Client" class is still the central
    location for client side shared connection settings like the
    client side mutual TLS. A new class "TransactionContext" is
    applied at each building of a proposal, avoiding the client
    class having user state and a state or crypto store.
    New ChannelDiscovery class added to be the focal point of discovery
    calls to the peer for a channel. This class will build
    and sign the discovery request. This class will
    decode the discover configurations and
    endorsement plans. It will provide peers and orderers to
    the channel along with the plan for the endorsement hsndler,
    query handler, and commit handler. The dicovered peers and
    organizations will be the source endpoint for event monitoring.
    
    Change-Id: If7dc29aaab2697d4979b36f209a23bfc43588d56
    Signed-off-by: Bret Harrison <beharrison@nc.rr.com>

---
+++ fabric-base/lib/TransactionContext.js
@@ -0,0 +1,101 @@
+/**
+ * Copyright 2019 IBM All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+const TYPE = 'TransactionContext';
+
+const {HashPrimitives, Utils} = require('fabric-common');
+
+const logger = Utils.getLogger(TYPE);
+const checkParameter = require('./Utils.js').checkParameter;
+
+
+/**
+ * @classdesc
+ * This class represents a TransactionContext, the user identity.
+ * This object will be used to provide the identity on outbound
+ * requests to the fabric network.
+ * <br><br>
+ * see the tutorial {@tutorial proposal}
+ * <br><br>
+ *
+ * @class
+ */
+const TransactionContext = class {
+
+	/**
+	 * Construct a ClientContext object.
+	 *
+	 * @param {User} user - The user identity instance
+	 * @returns {ClientContext} The ClientContext instance.
+	 */
+	constructor(user = checkParameter('user'), client = checkParameter('client')) {
+		this.type = TYPE;
+		this.client = client;
+		this.user = user;
+		this.options = {};
+	}
+
+	/**
+	 * Add options to be used when using this transaction context on a request
+	 * @param {string} name - The name of the attribute to add to the options list
+	 * @param {any} value - The value of the attribute
+	 */
+	addOption(name = checkParameter('name'), value = checkParameter('value')) {
+		this.options[name] = value;
+	}
+
+	/**
+	 * Resets the transaction ID and nonce values
+	 */
+	calculateTxId() {
+		const signer = this.user.getSigningIdentity();
+		this.nonce = Utils.getNonce(); // nonce is in bytes
+		const creator_bytes = signer.serialize();// same as signatureHeader.Creator
+		const trans_bytes = Buffer.concat([this.nonce, creator_bytes]);
+		const trans_hash = HashPrimitives.SHA2_256(trans_bytes);
+		this.txId = Buffer.from(trans_hash).toString();
+		logger.debug('calculateTxId - txId:%s', this.txId);
+	}
+
+	/**
+	 * Get the client certificate hash
+	 * @returns {byte[]} The hash of the client certificate
+	 */
+	getClientCertHash() {
+		let hash = this.client.getClientCertHash();
+		if (!hash) {
+			hash = this.client.getClientCertHash(this.user);
+		}
+
+		return hash;
+	}
+
+	/**
+	 * Get the protobuf serialized identity of this user
+	 * @returns {byte[]} serialized identity in bytes
+	 */
+	serializeIdentity() {
+		// This could a place where we will handle the different
+		// user types
+		return this.user.getIdentity().serialize();
+	}
+
+	/**
+	 * return a printable representation of this object
+	 */
+	toString() {
+		return 'TransactionContext: {' +
+			'user: ' + this.user.getName() +
+			'txId:' + this.txId +
+			'nonce:' + this.nonce +
+		'}';
+	}
+
+};
+
+module.exports = TransactionContext;
+module.exports.TYPE = TYPE;
---
+++ fabric-base/test/TransactionContext.js
@@ -0,0 +1,84 @@
+/**
+ * Copyright 2018 IBM All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+const fs = require('fs');
+const path = require('path');
+const rewire = require('rewire');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const sinonChai = require('sinon-chai');
+const should = chai.should();
+chai.use(chaiAsPromised);
+chai.use(sinonChai);
+const sinon = require('sinon');
+
+const {CryptoSuite, Key, Signer, SigningIdentity, Utils, User} = require('fabric-common');
+const TransactionContext = rewire('../lib/TransactionContext');
+
+const certificateAsPEM = fs.readFileSync(path.join(__dirname, '../../fabric-common/test/data', 'cert.pem'));
+const certificateAsBuffer = Buffer.from(certificateAsPEM);
+const certificateAsHex = certificateAsBuffer.toString('hex');
+const mspId = 'Org1MSP';
+
+describe('TransactionContext', () => {
+	Utils.setConfigSetting('crypto-suite-software', {
+		'EC': 'fabric-common/lib/impl/CryptoSuite_ECDSA_AES.js'
+	});
+	Utils.setConfigSetting('crypto-keysize', 256);
+	Utils.setConfigSetting('crypto-hash-algo', 'SHA2');
+
+	let user;
+	let signingIdentity;
+	let mockPublicKey;
+	let mockCryptoSuite;
+	let mockSigner;
+	let transactionContext;
+
+	beforeEach(() => {
+		mockPublicKey = sinon.createStubInstance(Key);
+		mockCryptoSuite = sinon.createStubInstance(CryptoSuite);
+		mockSigner = sinon.createStubInstance(Signer);
+		signingIdentity = new SigningIdentity(certificateAsHex, mockPublicKey, mspId, mockCryptoSuite, mockSigner);
+		user = new User('admin');
+		user.setSigningIdentity(signingIdentity);
+		user._mspId = mspId;
+		transactionContext = new TransactionContext(user);
+	});
+
+	describe('#constructor', () => {
+		it('should throw if no user', () => {
+			(() => {
+				new TransactionContext();
+			}).should.throw(/Missing user parameter/);
+		});
+		it('should throw if bad user', () => {
+			(() => {
+				new TransactionContext('bad');
+			}).should.throw(/Invalid user instance/);
+		});
+		it('should have a user name by now', () => {
+			should.equal(transactionContext.user.getName(), 'admin');
+		});
+	});
+
+	describe('#calculateTxId', () => {
+		it('should be able to reset the txId and nonce', () => {
+			const old_txId = transactionContext.txId;
+			const old_nonce = transactionContext.nonce;
+			transactionContext.calculateTxId();
+			should.not.equal(transactionContext.txId, old_txId);
+			should.not.equal(transactionContext.nonce, old_nonce);
+			should.equal(transactionContext.txId.length, 64);
+		});
+	});
+
+	describe('#addOption', () => {
+		it('should be able to add an option', () => {
+			transactionContext.addOption('op1', 'somevalue');
+			should.equal(transactionContext.options.op1, 'somevalue');
+		});
+	});
+});
