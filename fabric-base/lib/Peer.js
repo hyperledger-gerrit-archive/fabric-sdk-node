7c337417a3ba1c1f4ec86476fc1b32263acd801c
Bret Harrison <beharrison@nc.rr.com>
2019-06-04 09:08:30 -0400

    FABN-1347 NodeSDK fabric-base initial
    Initial files added to new module fabric-base.
    Some unit test and an integration test
    showing the complete flow of an endorsement, commit with
    monitoring for an event, query and discovery calls.
    
    Note that the channel class is not the focal point
    of the flow, the new class "Proposal" is the object that
    is used to build up the proposal, sign it, and then
    send it. Then again to build up the commit submission,
    using the results of the endorsement, sign it, then send
    it to the orderer. The "Client" class is still the central
    location for client side shared connection settings like the
    client side mutual TLS. A new class "TransactionContext" is
    applied at each building of a proposal, avoiding the client
    class having user state and a state or crypto store.
    New ChannelDiscovery class added to be the focal point of discovery
    calls to the peer for a channel. This class will build
    and sign the discovery request. This class will
    decode the discover configurations and
    endorsement plans. It will provide peers and orderers to
    the channel along with the plan for the endorsement hsndler,
    query handler, and commit handler. The dicovered peers and
    organizations will be the source endpoint for event monitoring.
    
    Change-Id: If7dc29aaab2697d4979b36f209a23bfc43588d56
    Signed-off-by: Bret Harrison <beharrison@nc.rr.com>
    Signed-off-by: david <david-khala@hotmail.com>

---
+++ fabric-base/lib/Peer.js
@@ -0,0 +1,181 @@
+/**
+ * Copyright 2019 IBM All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+const TYPE = 'Peer';
+
+const {Utils: utils} = require('fabric-common');
+const Remote = require('./Remote');
+const fabprotos = require('fabric-protos');
+
+const logger = utils.getLogger(TYPE);
+const {checkParameter} = require('./Utils.js');
+
+/**
+ * The Peer class represents a peer in the target blockchain network.
+ * The application can send endorsement proposals, and query requests through this
+ * class.
+ *
+ * @class
+ * @extends Remote
+ */
+class Peer extends Remote {
+
+	/**
+	 * Construct a Peer object with the name. A peer object encapsulates the
+	 * properties of an endorsing peer and the interactions with it
+	 * via the grpc service API. Peer objects are used by the {@link Client} objects to
+	 * send channel-agnostic requests such as installing chaincode, querying peers for
+	 * installed chaincodes, etc. They are also used by the {@link Channel} objects to
+	 * send channel-aware requests such as instantiating chaincodes, and invoking
+	 * transactions.
+	 * Use the connect method with options to establish a
+	 * connection with the fabric network endpoint.
+	 *
+	 * @param {string} name - The name of this peer
+	 * @param {Client} client - The client instance
+	 * @param {string} [mspid] - The mspid (organization) of this peer
+	 * @returns {Peer} The Peer instance.
+	 */
+	constructor(name = checkParameter('name'), client = checkParameter('client'), mspid) {
+		logger.debug(`${TYPE}.constructor[${name}] - start `);
+		super(name, client);
+		this.mspid = mspid;
+		this.type = TYPE;
+		this.endorserService = null;
+	}
+
+	/**
+	 * Connects to a Peer with the given url and opts.
+	 * If a connection exist it will be closed and replaced by
+	 * a new connection using the options provided.
+	 *
+	 * @param {Endpoint} endpoint - Service connection options including the url.
+	 * @param {ConnectionOpts} options - Any specific options for this instance
+	 *  of the connection to the peer. These will override options from the
+	 *  endpoint service connection options.
+	 */
+	async connect(endpoint = checkParameter('endpoint'), options = {}) {
+		const method = `connect[${this.name}]`;
+		logger.debug(`${method} - start `);
+
+		if (this.endorserService) {
+			logger.error('%s - endorser service exist for this peer %s', method, this.name);
+			throw Error(`This peer ${this.name} is connected`);
+		}
+
+		this.endpoint = endpoint;
+		this.options = Object.assign({}, endpoint.options, options);
+
+		logger.debug(`${method} - endorser service does not exist, will create service for this peer ${this.name}`);
+		this.endorserService = new fabprotos.protos.Endorser(this.endpoint.addr, this.endpoint.creds, this.options);
+		await this.waitForReady(this.endorserService);
+		logger.debug(`${method} - completed the waitForReady for this peer ${this.name}`);
+	}
+
+	/**
+	 * Check the connection status
+	 */
+	async checkConnection() {
+		logger.debug(`checkConnection[${this.name}] - start `);
+
+		if (this.connected) {
+			try {
+				await this.waitForReady(this.endorserService);
+			} catch (error) {
+				logger.error(`Peer ${this.endpoint.url} Connection failed :: ${error}`);
+			}
+		}
+
+		return this.connected;
+	}
+
+	/**
+	 * disconnect the service connection.
+	 */
+	disconnect() {
+		const method = `disconnect[${this.name}]`;
+		logger.debug(`${method} - start `);
+
+		if (this.endorserService) {
+			logger.debug(`${method} - closing peer endorser connection ${this.endpoint.addr}`);
+			this.endorserService.close();
+			this.endorserService = null;
+		}
+	}
+
+	/**
+	 * Send an endorsement proposal to an endorser. This is used to call an
+	 * endorsing peer to execute a chaincode to process a transaction proposal,
+	 * or runs queries.
+	 *
+	 * @param {Envelope} signedEnvelope - A signed proposal envelope that
+	 *  has been signed
+	 * @param {Number} timeout - A number indicating milliseconds to wait on the
+	 *  response before rejecting the promise with a timeout error. This
+	 *  overrides the request-timeout config connection setting of this instance.
+	 * @returns {Promise} A Promise for a {@link ProposalResponse}
+	 */
+	sendProposal(signedEnvelope = checkParameter('signedEnvelope'), timeout) {
+		const method = `sendProposal[${this.name}]`;
+		logger.debug(`${method} - Start ----${this.name} ${this.endpoint.url}`);
+
+		if (this.connected === false) {
+			throw Error(`Endorser Client ${this.name} ${this.endpoint.url} is not connected`);
+		}
+
+		let rto = this.options['request-timeout'];
+		if (typeof timeout === 'number') {
+			rto = timeout;
+		}
+
+		return new Promise((resolve, reject) => {
+			const send_timeout = setTimeout(() => {
+				clearTimeout(send_timeout);
+				logger.error(`${method} - ${this.name} timed out after:${rto}`);
+				return reject(new Error('REQUEST_TIMEOUT'));
+			}, rto);
+
+			this.endorserService.processProposal(signedEnvelope, (err, proposalResponse) => {
+				clearTimeout(send_timeout);
+				if (err) {
+					logger.debug(`${method} - Received proposal response from: ${this._url} status: ${err}`);
+					if (err instanceof Error) {
+						reject(err);
+					} else {
+						reject(new Error(err));
+					}
+				} else {
+					if (proposalResponse) {
+						logger.debug(`${method} - Received proposal response from peer "${this._url}": status - ${proposalResponse.response && proposalResponse.response.status}`);
+						// 400 is the error threshold level, anything below that the endorser will endorse it.
+						if (proposalResponse.response && proposalResponse.response.status < 400) {
+							proposalResponse.connection = this.getCharacteristics();
+							resolve(proposalResponse);
+						} else if (proposalResponse.response && proposalResponse.response.message) {
+							const error = Object.assign(new Error(proposalResponse.response.message), proposalResponse.response);
+							error.connection = this.getCharacteristics();
+							error.isProposalResponse = true;
+							reject(error);
+						} else {
+							const return_error = new Error(`GRPC service failed to get a proper response from the peer "${this._url}".`);
+							return_error.connection = this.getCharacteristics();
+							logger.error(`${method} - rejecting with:${return_error}`);
+							reject(return_error);
+						}
+					} else {
+						const return_error = new Error(`GRPC service got a null or undefined response from the peer "${this._url}".`);
+						return_error.connection = this.getCharacteristics();
+						logger.error(`${method} - rejecting with:${return_error}`);
+						reject(return_error);
+					}
+				}
+			});
+		});
+	}
+}
+
+module.exports = Peer;
---
+++ fabric-base/test/ChannelEventHub.js
@@ -0,0 +1,174 @@
+/**
+ * Copyright 2018 IBM All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+const fs = require('fs');
+const path = require('path');
+const rewire = require('rewire');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const sinonChai = require('sinon-chai');
+const expect = chai.expect;
+chai.use(chaiAsPromised);
+chai.use(sinonChai);
+const sinon = require('sinon');
+
+const fabprotos = require('fabric-protos');
+const Client = require('../lib/Client');
+const ChannelEventHub = rewire('../lib/ChannelEventHub');
+const {CryptoSuite, Key, Signer, SigningIdentity, Utils, User} = require('fabric-common');
+
+const certificateAsPEM = fs.readFileSync(path.join(__dirname, '../../fabric-common/test/data', 'cert.pem'));
+const certificateAsBuffer = Buffer.from(certificateAsPEM);
+const certificateAsHex = certificateAsBuffer.toString('hex');
+
+describe('ChannelEventHub', () => {
+	Utils.setConfigSetting('crypto-suite-software', {
+		'EC': 'fabric-common/lib/impl/CryptoSuite_ECDSA_AES.js'
+	});
+	Utils.setConfigSetting('crypto-keysize', 256);
+	Utils.setConfigSetting('crypto-hash-algo', 'SHA2');
+	const tls_mutual = {};
+	tls_mutual.clientCert = null;
+	tls_mutual.clientKey = null;
+	tls_mutual.clientCertHash = null;
+	tls_mutual.selfGenerated = false;
+
+	const mockPublicKey = sinon.createStubInstance(Key);
+	const mockCryptoSuite = sinon.createStubInstance(CryptoSuite);
+	const mockSigner = sinon.createStubInstance(Signer);
+	const signingIdentity = new SigningIdentity(certificateAsHex, mockPublicKey, 'org1', mockCryptoSuite, mockSigner);
+	const user = new User('admin');
+	user.setSigningIdentity(signingIdentity);
+	user._mspId = 'org1';
+
+	let client;
+	let txContext;
+	let channel;
+	let peer;
+	let hub;
+	let sandbox;
+
+	beforeEach(() => {
+		sandbox = sinon.createSandbox();
+
+		client = new Client('myclient');
+		client.clientCertHash = Buffer.from('hash');
+		txContext = client.newTransactionContext(user);
+		channel = client.newChannel('mychannel');
+		channel.buildChannelHeader = () => {
+			return new fabprotos.common.ChannelHeader();
+		};
+		peer = client.newPeer('peer1');
+		hub = channel.newChannelEventHub('myhub', channel, peer);
+	});
+
+	describe('#constructor', () => {
+		it('should require a name', () => {
+			(() => {
+				new ChannelEventHub();
+			}).should.throw('Missing name parameter');
+		});
+
+		it('should require a channel', () => {
+			(() => {
+				new ChannelEventHub('myhub');
+			}).should.throw('Missing channel parameter');
+		});
+
+		it('should require a peer', () => {
+			(() => {
+				new ChannelEventHub('myhub', channel);
+			}).should.throw('Missing peer parameter');
+		});
+
+		it('should be able to create a channel event hub', () => {
+			hub.name.should.equal('myhub');
+		});
+	});
+
+	describe('#connect', () => {
+		it('should get the correct options', () => {
+			const fakeStub = sandbox.stub().returns('done');
+			hub.waitForReady = fakeStub;
+			hub.connect();
+			hub.connect({test:'test'});
+			hub.options.test.should.equal('test');
+			return;
+		});
+	});
+
+	describe('#buildStartRequest', () => {
+		it('should require a txContext', () => {
+			(() => {
+				hub.buildStartRequest();
+			}).should.throw('Missing txContext parameter');
+		});
+
+		it('should get the correct defaults', () => {
+			hub.buildStartRequest(txContext);
+			hub.filtered.should.equal(true);
+			hub.start_block.should.equal('newest');
+			hub.end_block_seen.should.equal(false);
+		});
+		it('should get the correct settings', () => {
+			hub.buildStartRequest(txContext, {filtered: false, startBlock: 'oldest', endBlock: 4});
+			hub.filtered.should.equal(false);
+			hub.start_block.should.equal('oldest');
+			hub.end_block.should.equal(4);
+			hub.end_block_seen.should.equal(false);
+		});
+	});
+
+	describe('#setStartRequestSignature', () => {
+		it('should require a signature', () => {
+			(() => {
+				hub.setStartRequestSignature();
+			}).should.throw('Missing signature parameter');
+		});
+	});
+
+	describe('#signStartRequest', () => {
+		it('should require a txContext', () => {
+			(() => {
+				hub.signStartRequest();
+			}).should.throw('Missing txContext parameter');
+		});
+		it('should require a payload_bytes', () => {
+			(() => {
+				hub.signStartRequest({});
+			}).should.throw('Missing payload_bytes parameter');
+		});
+	});
+
+	describe('#buildAndSignStartRequest', () => {
+		it('should require a txContext', () => {
+			(() => {
+				hub.signStartRequest();
+			}).should.throw('Missing txContext parameter');
+		});
+	});
+
+	describe('#getSignedStartRequestEnvelope', () => {
+		it('should require a payload', () => {
+			(() => {
+				hub.getSignedStartRequestEnvelope();
+			}).should.throw('Missing payload - build the start request');
+		});
+		it('should require a signature', () => {
+			(() => {
+				hub.seekPayloadBytes = 'payload';
+				hub.getSignedStartRequestEnvelope();
+			}).should.throw('Missing signature - sign the start request');
+		});
+		it('should get the correct envelope', () => {
+			hub.seekPayloadBytes = 'payload';
+			hub.signature = 'signature';
+			const envelope = hub.getSignedStartRequestEnvelope();
+			expect(envelope.signature).to.equal('signature');
+			expect(envelope.payload).to.equal('payload');
+		});
+	});
+});
