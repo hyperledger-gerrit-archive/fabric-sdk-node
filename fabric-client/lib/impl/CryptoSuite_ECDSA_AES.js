924d7b18271d645c8aa6da86acc77bc27cddb395
Hengming Zhang <fiveswind@gmail.com>
2019-09-05 15:25:43 +0800

    [FABN-1337] keyStore class design refactor
    The CryptoKeyStore.js has been got a refactoring and becomes
    a much simpler since the object-oriented class design.
    
    This commit does below:
    1. Refactor CryptoKeyStore.js and rewrite crypto-key-store.js
    unit test to ES6 above form grammar.
    2. Refactor CryptoKeyStore in Utils.js and utils.js test.
    3. Fix CouchDBKeyValueStore's declaration bug and its test failure.
    4. Remove _getKeyStore() method in CryptoSuite_ECDSA_AES.js.
    
    Signed-off-by: “5sWind” <fiveswind@gmail.com>
    Change-Id: I30e56123055488cfcfb3a4c4a23159c5442897d4
    Signed-off-by: david <david-khala@hotmail.com>

--- fabric-client/lib/impl/CryptoSuite_ECDSA_AES.js
+++ fabric-client/lib/impl/CryptoSuite_ECDSA_AES.js
@@ -99,23 +99,14 @@ class CryptoSuite_ECDSA_AES extends api.CryptoSuite {
 	}
 
 	async generateKey(opts) {
-		const pair = KEYUTIL.generateKeypair('EC', this._curveName);
-
-		if (typeof opts !== 'undefined' && typeof opts.ephemeral !== 'undefined' && opts.ephemeral === true) {
-			logger.debug('generateKey, ephemeral true, Promise resolved');
-			return new ECDSAKey(pair.prvKeyObj);
-		} else {
-			if (!this._cryptoKeyStore) {
-				throw new Error('generateKey opts.ephemeral is false, which requires CryptoKeyStore to be set.');
-			}
-			// unless "opts.ephemeral" is explicitly set to "true", default to saving the key
-			const key = new ECDSAKey(pair.prvKeyObj);
-
-			const store = await this._cryptoKeyStore._getKeyStore();
-			logger.debug('generateKey, store.setValue');
-			await store.putKey(key);
-			return key;
+		if (!this._cryptoKeyStore) {
+			throw new Error('generateKey requires CryptoKeyStore to be set.');
 		}
+		const key = this.generateEphemeralKey();
+		const store = await this._cryptoKeyStore;
+		logger.debug('generateKey, store.putKey');
+		await store.putKey(key);
+		return key;
 	}
 
 	/**
@@ -129,7 +120,7 @@ class CryptoSuite_ECDSA_AES extends api.CryptoSuite {
 	/**
 	 * This is an implementation of {@link module:api.CryptoSuite#importKey}
 	 */
-	importKey(pem, opts) {
+	async importKey(pem, opts) {
 		logger.debug('importKey - start');
 		let store_key = true; // default
 		if (typeof opts !== 'undefined' && typeof opts.ephemeral !== 'undefined' && opts.ephemeral === true) {
@@ -139,7 +130,6 @@ class CryptoSuite_ECDSA_AES extends api.CryptoSuite {
 			throw new Error('importKey opts.ephemeral is false, which requires CryptoKeyStore to be set.');
 		}
 
-		const self = this;
 		// attempt to import the raw content, assuming it's one of the following:
 		// X.509v1/v3 PEM certificate (RSA/DSA/ECC)
 		// PKCS#8 PEM RSA/DSA/ECC public key
@@ -177,17 +167,9 @@ class CryptoSuite_ECDSA_AES extends api.CryptoSuite {
 				logger.error('importKey - %j', error);
 				return Promise.reject(error);
 			}
-			return new Promise((resolve, reject) => {
-				return self._cryptoKeyStore._getKeyStore()
-					.then((store) => {
-						return store.putKey(theKey);
-					}).then(() => {
-						return resolve(theKey);
-					}).catch((err) => {
-						reject(err);
-					});
-
-			});
+			const store = await this._cryptoKeyStore;
+			await store.putKey(theKey);
+			return theKey;
 		}
 	}
 
@@ -196,7 +178,7 @@ class CryptoSuite_ECDSA_AES extends api.CryptoSuite {
 		if (!this._cryptoKeyStore) {
 			throw new Error('getKey requires CryptoKeyStore to be set.');
 		}
-		const store = await this._cryptoKeyStore._getKeyStore();
+		const store = await this._cryptoKeyStore;
 		const key = await store.getKey(ski);
 		if (key instanceof ECDSAKey) {
 			return key;
