b64712096f08af305d9e6a5cfcebb708eb08bfe2
Hengming Zhang <fiveswind@gmail.com>
2019-09-22 10:05:26 +0800

    [FABN-1382] Use ES7 syntax as much as possible
    1/2 of CryptoKeyStore refactoring process.
    - CouchDBKeyValueStore
    - FileKeyValueStore
    
    and their tests.
    
    Signed-off-by: Hengming Zhang <fiveswind@gmail.com>
    Change-Id: I371ce194564f84815e04a2073c03e91b4801e3e8

--- test/unit/file-key-value-store.js
+++ test/unit/file-key-value-store.js
@@ -22,101 +22,82 @@ const testKey = 'keyValFileStoreName';
 const testValue = 'secretKeyValue';
 let store1 = '';
 
-test('\n\n ** FileKeyValueStore - read and write test **\n\n', (t) => {
+test('\n\n ** FileKeyValueStore - read and write test **\n\n', async (t) => {
 	testUtil.resetDefaults();
 
 	// clean up
 	if (testUtil.existsSync(keyValStorePath)) {
 		fs.removeSync(keyValStorePath);
 	}
-	utils.newKeyValueStore({
-		path: keyValStorePath
-	})
-		.then(
-			(store) => {
-				if (testUtil.existsSync(keyValStorePath)) {
-					t.pass('FileKeyValueStore read and write test: Successfully created new directory for testValueStore');
-				} else {
-					t.fail('FileKeyValueStore read and write test: failed to create new directory for testValueStore');
+	try {
+		let store = await utils.newKeyValueStore({path: keyValStorePath});
+		if (testUtil.existsSync(keyValStorePath)) {
+			t.pass('FileKeyValueStore read and write test: Successfully created new directory for testValueStore');
+		} else {
+			t.fail('FileKeyValueStore read and write test: failed to create new directory for testValueStore');
+			t.end();
+		}
+		store1 = store;
+		try {
+			const result = await store.setValue(testKey, testValue);
+			if (result === testValue) {
+				t.pass('FileKeyValueStore read and write test: Successfully set value');
+			} else {
+				t.fail('FileKeyValueStore read and write test: set value ' + result + 'does not match testValue ' + testValue);
+				t.end();
+			}
+			if (testUtil.existsSync(path.join(keyValStorePath, testKey))) {
+				t.pass('FileKeyValueStore read and write test: Verified the file for key ' + testKey + ' does exist');
+
+				try {
+					let val = await store1.getValue(testKey);
+					if (val !== testValue) {
+						t.fail('FileKeyValueStore read and write test: get value ' + val + ' does not equal testValue of ' + testValue);
+					} else {
+						t.pass('FileKeyValueStore read and write test: Successfully retrieved value');
+					}
+
+					// now test getValue() when the underlying directory get deleted
+					fs.removeSync(keyValStorePath);
+					try {
+						val = await store1.getValue(testKey);
+
+						if (val === null) {
+							t.pass('FileKeyValueStore error check tests:  Delete store & getValue test. getValue() returns null as expected');
+						} else {
+							t.fail('FileKeyValueStore error check tests:  Delete store & getValue test. getValue() should not have returned value: ' + val);
+						}
+
+						store = await new FileKeyValueStore({path: keyValStorePath});
+						// now test setValue() when the underlying directory get deleted
+						fs.removeSync(keyValStorePath);
+						try {
+							await store.setValue(testKey, testValue);
+							t.fail('FileKeyValueStore error check tests:  Delete store & setValue test.  setValue() should have failed.');
+							t.end();
+						} catch (reason) {
+							t.pass('FileKeyValueStore error check tests:  Delete store & setValue test.  setValue() failed as expected: ' + reason);
+							t.end();
+						}
+					} catch (reason) {
+						t.fail('FileKeyValueStore error check tests:  Delete store & getValue test. getValue caught unexpected error: ' + reason);
+					}
+				} catch (reason) {
+					t.fail('FileKeyValueStore read and write test: Failed getValue, reason: ' + reason);
 					t.end();
 				}
-				store1 = store;
-				return store.setValue(testKey, testValue);
-			}
-		).then(
-			(result) => {
-				if (result === testValue) {
-					t.pass('FileKeyValueStore read and write test: Successfully set value');
-				} else {
-					t.fail('FileKeyValueStore read and write test: set value ' + result + 'does not match testValue ' + testValue);
-					t.end();
-				}
-				if (testUtil.existsSync(path.join(keyValStorePath, testKey))) {
-					t.pass('FileKeyValueStore read and write test: Verified the file for key ' + testKey + ' does exist');
-
-					return store1.getValue(testKey);
-				} else {
-					t.fail('FileKeyValueStore read and write test: Failed to create file for key ' + testKey);
-					t.end();
-				}
-			},
-			(reason) => {
-				t.fail('FileKeyValueStore read and write test: Failed to set value, reason: ' + reason);
+			} else {
+				t.fail('FileKeyValueStore read and write test: Failed to create file for key ' + testKey);
 				t.end();
 			}
-		).then(
-		// Log the fulfillment value
-			(val) => {
-				if (val !== testValue) {
-					t.fail('FileKeyValueStore read and write test: get value ' + val + ' does not equal testValue of ' + testValue);
-				} else {
-					t.pass('FileKeyValueStore read and write test: Successfully retrieved value');
-				}
-
-				// now test getValue() when the underlying directory get deleted
-				fs.removeSync(keyValStorePath);
-				return store1.getValue(testKey);
-			},
-			// Log the rejection reason
-			(reason) => {
-				t.fail('FileKeyValueStore read and write test: Failed getValue, reason: ' + reason);
-				t.end();
-			}
-		).then(
-		// Log the fulfillment value
-			(val) => {
-				if (val === null) {
-					t.pass('FileKeyValueStore error check tests:  Delete store & getValue test. getValue() returns null as expected');
-				} else {
-					t.fail('FileKeyValueStore error check tests:  Delete store & getValue test. getValue() should not have returned value: ' + val);
-				}
-
-				return new FileKeyValueStore({path: keyValStorePath});
-			},
-			(reason) => {
-				t.fail('FileKeyValueStore error check tests:  Delete store & getValue test. getValue caught unexpected error: ' + reason);
-			}
-		)
-		.then(
-			(store) => {
-			// now test setValue() when the underlying directory get deleted
-				fs.removeSync(keyValStorePath);
-				return store.setValue(testKey, testValue);
-			})
-		.then(
-			() => {
-				t.fail('FileKeyValueStore error check tests:  Delete store & setValue test.  setValue() should have failed.');
-				t.end();
-			},
-			(reason) => {
-				t.pass('FileKeyValueStore error check tests:  Delete store & setValue test.  setValue() failed as expected: ' + reason);
-				t.end();
-			})
-		.catch(
-			(err) => {
-				t.fail('Failed with unexpected error: ' + err.stack ? err.stack : err);
-				t.end();
-			});
+		} catch (reason) {
+			t.fail('FileKeyValueStore read and write test: Failed to set value, reason: ' + reason);
+			t.end();
+		}
+	} catch (err) {
+		t.fail('Failed with unexpected error: ' + err.stack ? err.stack : err);
+		t.end();
+	}
 });
 
 test('\n\n** FileKeyValueStore error check tests **\n\n', (t) => {
