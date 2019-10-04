a5041d048372d6bf8372b87860c94871e7783f83
Hengming Zhang <fiveswind@gmail.com>
2019-10-05 03:07:57 +0800

    [FABN-1337] keyStore class design refactor
    2/2 the process of refactoring related to FABN-1382.
    
    The CryptoKeyStore.js has been got a refactoring and becomes
    a much simpler since the object-oriented class design.
    
    The redesign of the CryptoKeyStore as of the release-1.4 branch.
    
    Signed-off-by: “5sWind” <fiveswind@gmail.com>
    Change-Id: I30e56123055488cfcfb3a4c4a23159c5442897d4
    Signed-off-by: david <david-khala@hotmail.com>

--- fabric-client/lib/impl/CryptoKeyStore.js
+++ fabric-client/lib/impl/CryptoKeyStore.js
@@ -6,89 +6,91 @@
 */
 
 'use strict';
-
+const {KeyValueStore} = require('../api');
 const jsrsasign = require('jsrsasign');
 const KEYUTIL = jsrsasign.KEYUTIL;
 
 const utils = require('../utils.js');
 const ECDSAKey = require('./ecdsa/key.js');
 
-/*
- * The mixin enforces the special indexing mechanism with private and public
- * keys on top of a standard implementation of the KeyValueStore interface
- * with the getKey() and putKey() methods
- */
-const CryptoKeyStoreMixin = (KeyValueStore) => class extends KeyValueStore {
-	getKey(ski) {
-		const self = this;
-
-		// first try the private key entry, since it encapsulates both
-		// the private key and public key
-		return this.getValue(_getKeyIndex(ski, true))
-			.then((raw) => {
-				if (raw !== null) {
-					const privKey = KEYUTIL.getKeyFromPlainPrivatePKCS8PEM(raw);
-					// TODO: for now assuming ECDSA keys only, need to add support for RSA keys
-					return new ECDSAKey(privKey);
-				}
-
-				// didn't find the private key entry matching the SKI
-				// next try the public key entry
-				return self.getValue(_getKeyIndex(ski, false));
-			}).then((key) => {
-				if (key instanceof ECDSAKey) {
-					return key;
-				}
-
-				if (key !== null) {
-					const pubKey = KEYUTIL.getKey(key);
-					return new ECDSAKey(pubKey);
-				}
-			});
-	}
-
-	putKey(key) {
-		const idx = _getKeyIndex(key.getSKI(), key.isPrivate());
-		const pem = key.toBytes();
-		return this.setValue(idx, pem)
-			.then(() => {
-				return key;
-			});
-	}
-};
+const logger = utils.getLogger('CryptoKeyStore.js');
 
 /**
  * A CryptoKeyStore uses an underlying instance of {@link module:api.KeyValueStore} implementation
  * to persist crypto keys.
  *
- * @param {function} KVSImplClass Optional. The built-in key store saves private keys.
- *    The key store may be backed by different {@link KeyValueStore} implementations.
- *    If specified, the value of the argument must point to a module implementing the
- *    KeyValueStore interface.
- * @param {Object} opts Implementation-specific option object used in the constructor
+ * This also enforces the special indexing mechanism with private and public
+ * keys on top of a standard implementation of the KeyValueStore interface
+ * with the getKey() and putKey() methods.
  *
  * @class
+ * @extends module:api.KeyValueStore
  */
-const CryptoKeyStore = function(KVSImplClass, opts) {
-	let superClass;
+const CryptoKeyStore = class extends KeyValueStore {
 
-	if (typeof KVSImplClass !== 'function') {
-		let impl_class = utils.getConfigSetting('crypto-value-store');
-		if (!impl_class) {
-			impl_class = utils.getConfigSetting('key-value-store');
+	/**
+	 * @constructor
+	 * @param {KeyValueStore} KVSInstance The key store instance saves private keys.
+	 *    The key store must be a instance implementing the KeyValueStore interface {@link KeyValueStore}
+	 */
+	constructor(KVSImplClass, opts) {
+		logger.debug('constructor', {KVSImplClass: KVSImplClass});
+
+		// Create KeyValueStore instance
+		super();
+
+		if (!KVSImplClass || !(KVSImplClass.prototype instanceof KeyValueStore)) {
+			if (KVSImplClass) {
+				opts = KVSImplClass;
+			}
+			KVSImplClass = require(utils.getConfigSetting('key-value-store'));
 		}
-		superClass = require(impl_class);
-	} else {
-		superClass = KVSImplClass;
+		if (!opts) {
+			opts = {path: utils.getDefaultKeyStorePath()};
+		}
+		return (async () => {
+			this._kvsInstance = await new KVSImplClass(opts);
+			return this;
+		})();
 	}
 
-	if (KVSImplClass !== null && typeof opts === 'undefined') {
-		// the function is called with only one argument for the 'opts'
-		opts = KVSImplClass;
+	async getValue(name) {
+		return await this._kvsInstance.getValue(name);
 	}
 
-	const MyClass = class extends CryptoKeyStoreMixin(superClass) {};
-	return new MyClass(opts);
+	async setValue(name, value) {
+		return await this._kvsInstance.setValue(name, value);
+	}
+
+	async getKey(ski) {
+		// first try the private key entry, since it encapsulates both
+		// the private key and public key
+		const raw = await this.getValue(_getKeyIndex(ski, true));
+		if (raw !== null) {
+			const privKey = KEYUTIL.getKeyFromPlainPrivatePKCS8PEM(raw);
+			// TODO: for now assuming ECDSA keys only, need to add support for RSA keys
+			return new ECDSAKey(privKey);
+		}
+
+		// didn't find the private key entry matching the SKI
+		// next try the public key entry
+		const key = await this.getValue(_getKeyIndex(ski, false));
+		if (key instanceof ECDSAKey) {
+			return key;
+		}
+
+		if (key !== null) {
+			const pubKey = KEYUTIL.getKey(key);
+			return new ECDSAKey(pubKey);
+		}
+	}
+
+	async putKey(key) {
+		const idx = _getKeyIndex(key.getSKI(), key.isPrivate());
+		const pem = key.toBytes();
+		await this.setValue(idx, pem);
+		return key;
+	}
 };
 
 function _getKeyIndex(ski, isPrivateKey) {
